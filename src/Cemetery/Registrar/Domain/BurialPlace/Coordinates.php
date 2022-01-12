81568e1805c93aac42aa54909a0d85df7c3e292d
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-12 19:22:22 +0700

    add value object for coordinates

---
+++ src/Cemetery/Registrar/Domain/BurialPlace/Coordinates.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\BurialPlace;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class Coordinates
+{
+    private const COORDINATE_VALUE_PATTERN = '~^[+/\-]?\d+\.?\d+$~';    // examples: 54.950357, -165.1282, etc.
+    private const ACCURACY_VALUE_PATTERN   = '~^\d+\.\d+$~';            // examples: 0.25, 12.5, etc.
+
+    /**
+     * @param string      $latitude
+     * @param string      $longitude
+     * @param string|null $accuracy
+     */
+    public function __construct(
+        private string  $latitude,
+        private string  $longitude,
+        private ?string $accuracy = null,
+    ) {
+        $this->assertValidLatitude($this->latitude);
+        $this->assertValidLongitude($this->longitude);
+        if ($this->accuracy) {
+            $this->assertValidAccuracy($this->accuracy);
+        }
+        $this->latitude  = \ltrim($this->latitude, '+');
+        $this->longitude = \ltrim($this->longitude, '+');
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return \sprintf('%s, %s', $this->latitude, $this->longitude);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLatitude(): string
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLongitude(): string
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAccuracy(): ?string
+    {
+        return $this->accuracy;
+    }
+
+    /**
+     * @param self $coordinates
+     *
+     * @return bool
+     */
+    public function isEqual(self $coordinates): bool
+    {
+        $isSameLatitude  = $coordinates->getLatitude()  === $this->getLatitude();
+        $isSameLongitude = $coordinates->getLongitude() === $this->getLongitude();
+        $isSameAccuracy  = $coordinates->getAccuracy()  === $this->getAccuracy();
+
+        return $isSameLatitude && $isSameLongitude && $isSameAccuracy;
+    }
+
+    /**
+     * @param string $latitude
+     *
+     * @throws \InvalidArgumentException when the latitude value is invalid
+     */
+    private function assertValidLatitude(string $latitude): void
+    {
+        $isValidFormat        = \preg_match(self::COORDINATE_VALUE_PATTERN, $latitude);
+        $isInsideOfValidRange = (float) $latitude >= -90.0 && (float) $latitude <= 90.0;
+        if (!$isValidFormat || !$isInsideOfValidRange) {
+            throw new \InvalidArgumentException(\sprintf('Invalid latitude value "%s".', $latitude));
+        }
+    }
+
+    /**
+     * @param string $longitude
+     *
+     * @throws \InvalidArgumentException when the longitude value is invalid
+     */
+    private function assertValidLongitude(string $longitude): void
+    {
+        $isValidFormat        = \preg_match(self::COORDINATE_VALUE_PATTERN, $longitude);
+        $isInsideOfValidRange = (float) $longitude >= -180.0 && (float) $longitude <= 180.0;
+        if (!$isValidFormat || !$isInsideOfValidRange) {
+            throw new \InvalidArgumentException(\sprintf('Invalid longitude value "%s".', $longitude));
+        }
+    }
+
+    /**
+     * @param string $accuracy
+     *
+     * @throws \InvalidArgumentException when the accuracy value is invalid
+     */
+    private function assertValidAccuracy(string $accuracy): void
+    {
+        $isValidFormat        = \preg_match(self::ACCURACY_VALUE_PATTERN, $accuracy);
+        $isInsideOfValidRange = (float) $accuracy >= 0.0;
+        if (!$isValidFormat || !$isInsideOfValidRange) {
+            throw new \InvalidArgumentException(\sprintf('Invalid accuracy value "%s".', $accuracy));
+        }
+    }
+}
+
---
+++ tests/Cemetery/Registrar/Domain/BurialPlace/CoordinatesTest.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\BurialPlace;
+
+use Cemetery\Registrar\Domain\BurialPlace\Coordinates;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class CoordinatesTest extends TestCase
+{
+    public function testItSuccessfullyCreated(): void
+    {
+        $coordinates = new Coordinates('54.950357', '82.7972252', '0.89');
+        $this->assertSame($coordinates->getLatitude(), '54.950357');
+        $this->assertSame($coordinates->getLongitude(), '82.7972252');
+        $this->assertSame($coordinates->getAccuracy(), '0.89');
+
+        $coordinates = new Coordinates('54.950357', '82.7972252');
+        $this->assertSame($coordinates->getLatitude(), '54.950357');
+        $this->assertSame($coordinates->getLongitude(), '82.7972252');
+        $this->assertNull($coordinates->getAccuracy());
+
+        $coordinates = new Coordinates('+54.950357', '+82.7972252', '0.89');
+        $this->assertSame($coordinates->getLatitude(), '54.950357');
+        $this->assertSame($coordinates->getLongitude(), '82.7972252');
+        $this->assertSame($coordinates->getAccuracy(), '0.89');
+
+        $coordinates = new Coordinates('-54.950357', '-82.7972252');
+        $this->assertSame($coordinates->getLatitude(), '-54.950357');
+        $this->assertSame($coordinates->getLongitude(), '-82.7972252');
+        $this->assertNull($coordinates->getAccuracy());
+    }
+
+    public function testItFailsWithInvalidLatitudeValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid latitude value "91.5".');
+        new Coordinates('91.5', '82.7972252');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid latitude value "-91.5".');
+        new Coordinates('-91.5', '82.7972252');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid latitude value "54.9A".');
+        new Coordinates('54.9A', '82.7972252');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid latitude value "54".');
+        new Coordinates('54', '82.7972252');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid latitude value ".950357".');
+        new Coordinates('.950357', '82.7972252');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid latitude value "".');
+        new Coordinates('', '82.7972252');
+    }
+
+    public function testItFailsWithInvalidLongitudeValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid longitude value "182.7".');
+        new Coordinates('54.950357', '182.7');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid longitude value "-182.7".');
+        new Coordinates('54.950357', '-182.7');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid longitude value "82.7B".');
+        new Coordinates('54.950357', '82.7B');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid longitude value "82".');
+        new Coordinates('54.950357', '82');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid longitude value ".734".');
+        new Coordinates('54.950357', '.734');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid longitude value "".');
+        new Coordinates('54.950357', '');
+    }
+
+    public function testItFailsWithInvalidAccuracyValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid accuracy value "0".');
+        new Coordinates('54.950357', '82.7972252', '0');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid accuracy value "-1.7".');
+        new Coordinates('54.950357', '82.7972252', '-1.7');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid accuracy value "".');
+        new Coordinates('54.950357', '82.7972252', '');
+    }
+
+    public function testItStringifyable(): void
+    {
+        $coordinates = new Coordinates('54.950357', '82.7972252', '0.89');
+        $this->assertSame('54.950357, 82.7972252', (string) $coordinates);
+
+        $coordinates = new Coordinates('54.950357', '82.7972252');
+        $this->assertSame('54.950357, 82.7972252', (string) $coordinates);
+    }
+
+    public function testItComparable(): void
+    {
+        $coordinatesA  = new Coordinates('54.950357', '82.7972252', '0.89');
+        $coordinatesB  = new Coordinates('54.950357', '82.7972252', '0.89');
+        $coordinatesC1 = new Coordinates('44.950357', '82.7972252', '0.89');
+        $coordinatesC2 = new Coordinates('54.950357', '72.7972252', '0.89');
+        $coordinatesC3 = new Coordinates('54.950357', '72.7972252', '1.2');
+        $coordinatesC4 = new Coordinates('54.950357', '82.7972252');
+
+        $this->assertTrue($coordinatesA->isEqual($coordinatesB));
+        $this->assertFalse($coordinatesA->isEqual($coordinatesC1));
+        $this->assertFalse($coordinatesA->isEqual($coordinatesC2));
+        $this->assertFalse($coordinatesA->isEqual($coordinatesC3));
+        $this->assertFalse($coordinatesA->isEqual($coordinatesC4));
+    }
+}
