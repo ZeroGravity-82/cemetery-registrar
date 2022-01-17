d53bc8f206647f558c388fad9d26533815d60a19
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-18 00:24:57 +0700

    reformat tests for burial collection

--- tests/Cemetery/Registrar/Domain/Burial/BurialCollectionTest.php
+++ tests/Cemetery/Registrar/Domain/Burial/BurialCollectionTest.php
@@ -25,18 +25,18 @@ class BurialCollectionTest extends AbstractEntityCollectionTest
         $naturalPersonIdC = new NaturalPersonId('NP004');
         $naturalPersonIdD = new NaturalPersonId('NP005');
         $customerId       = new CustomerId('C001', CustomerType::naturalPerson());
-        $siteIdA       = new SiteId('S001');
-        $siteIdB       = new SiteId('S002');
-        $siteIdC       = new SiteId('S003');
-        $siteIdD       = new SiteId('S004');
-        $this->idA     = new BurialId('B001');
-        $this->idB     = new BurialId('B002');
-        $this->idC     = new BurialId('B003');
-        $this->idD     = new BurialId('B004');
-        $this->entityA = new Burial($this->idA, $naturalPersonIdA, $customerId, $siteIdA, null);
-        $this->entityB = new Burial($this->idB, $naturalPersonIdB, $customerId, $siteIdB, $naturalPersonIdB);
-        $this->entityC = new Burial($this->idC, $naturalPersonIdC, $customerId, $siteIdC, $naturalPersonIdC);
-        $this->entityD = new Burial($this->idD, $naturalPersonIdD, $customerId, $siteIdD, null);
+        $siteIdA          = new SiteId('S001');
+        $siteIdB          = new SiteId('S002');
+        $siteIdC          = new SiteId('S003');
+        $siteIdD          = new SiteId('S004');
+        $this->idA        = new BurialId('B001');
+        $this->idB        = new BurialId('B002');
+        $this->idC        = new BurialId('B003');
+        $this->idD        = new BurialId('B004');
+        $this->entityA    = new Burial($this->idA, $naturalPersonIdA, $customerId, $siteIdA, null);
+        $this->entityB    = new Burial($this->idB, $naturalPersonIdB, $customerId, $siteIdB, $naturalPersonIdB);
+        $this->entityC    = new Burial($this->idC, $naturalPersonIdC, $customerId, $siteIdC, $naturalPersonIdC);
+        $this->entityD    = new Burial($this->idD, $naturalPersonIdD, $customerId, $siteIdD, null);
         $this->collection = new BurialCollection([$this->entityA]);
     }
 
