012be3a9eecdba0210d0ac46d920571acaaef051
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-20 16:59:27 +0700

    add test for ClosureNullator class

---
+++ tests/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/ORM/NullableEmbeddable/TestNullableEmbeddable.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\ORM\NullableEmbeddable;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ * @see    https://github.com/tarifhaus/doctrine-nullable-embeddable
+ */
+final class TestNullableEmbeddable
+{
+    /**
+     * @param bool $foo
+     */
+    public function __construct(
+        private bool $foo,
+    ) {}
+
+    public function getFoo(): bool
+    {
+        return $this->foo;
+    }
+}
