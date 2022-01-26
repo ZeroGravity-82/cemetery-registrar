9cd3de0b3e63d096cecb879acaa9d8ef5443d395
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-26 15:21:35 +0700

    add ColumbariumNicheId value object

---
+++ src/Cemetery/Registrar/Domain/BurialPlace/ColumbariumNicheId.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\BurialPlace;
+
+use Cemetery\Registrar\Domain\AbstractEntityId;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class ColumbariumNicheId extends AbstractEntityId
+{
+}
---
+++ tests/Cemetery/Registrar/Domain/BurialPlace/ColumbariumNicheIdTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\BurialPlace;
+
+use Cemetery\Registrar\Domain\BurialPlace\ColumbariumNicheId;
+use Cemetery\Tests\Registrar\Domain\AbstractEntityIdTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class ColumbariumNicheIdTest extends AbstractEntityIdTest
+{
+    protected string $className = ColumbariumNicheId::class;
+}
