487b3ed7a48b25359a7d9a9ffcbf28b200ba94ad
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-26 15:14:49 +0700

    rename and move GraveSideId value object

--- src/Cemetery/Registrar/Domain/BurialPlace/GraveSiteId.php
+++ src/Cemetery/Registrar/Domain/BurialPlace/GraveSiteId.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-namespace Cemetery\Registrar\Domain\Site;
+namespace Cemetery\Registrar\Domain\BurialPlace;
 
 use Cemetery\Registrar\Domain\AbstractEntityId;
 
 /**
  * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
  */
-final class SiteId extends AbstractEntityId
+final class GraveSiteId extends AbstractEntityId
 {
 }
