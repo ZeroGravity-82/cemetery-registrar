b2889f7f7e0cdb3cb35094ef62d147882acf6fe2
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-27 12:11:10 +0700

    add BurialContainerType value object

---
+++ tests/Cemetery/Registrar/Domain/Burial/BurialContainerTypeTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Burial;
+
+use Cemetery\Registrar\Domain\Burial\BurialContainerType;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class BurialContainerTypeTest extends TestCase
+{
+    public function testItSuccessfullyCreated(): void
+    {
+        $burialContainerType = new BurialContainerType(BurialContainerType::COFFIN);
+        $this->assertSame(BurialContainerType::COFFIN, $burialContainerType->getValue());
+        $this->assertTrue($burialContainerType->isCoffin());
+        $this->assertFalse($burialContainerType->isUrn());
+
+        $burialContainerType = BurialContainerType::urn();
+        $this->assertSame(BurialContainerType::URN, $burialContainerType->getValue());
+        $this->assertFalse($burialContainerType->isCoffin());
+        $this->assertTrue($burialContainerType->isUrn());
+    }
+
+    public function testItFailsWithUnsupportedValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(\sprintf(
+            'Unsupported burial container type "wrong_type", expected to be one of "%s", "%s".',
+            BurialContainerType::COFFIN,
+            BurialContainerType::URN,
+        ));
+        new BurialContainerType('wrong_type');
+    }
+
+    public function testItStringifyable(): void
+    {
+        $burialContainerType = BurialContainerType::coffin();
+
+        $this->assertSame(BurialContainerType::COFFIN, (string) $burialContainerType);
+    }
+
+    public function testItComparable(): void
+    {
+        $burialContainerTypeA = BurialContainerType::coffin();
+        $burialContainerTypeB = BurialContainerType::urn();
+        $burialContainerTypeC = BurialContainerType::coffin();
+
+        $this->assertFalse($burialContainerTypeA->isEqual($burialContainerTypeB));
+        $this->assertTrue($burialContainerTypeA->isEqual($burialContainerTypeC));
+        $this->assertFalse($burialContainerTypeB->isEqual($burialContainerTypeC));
+    }
+}
