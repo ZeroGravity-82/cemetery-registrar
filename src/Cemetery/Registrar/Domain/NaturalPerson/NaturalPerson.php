a3725ef21c0b61170603ce511d6178dc43e57e36
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-27 15:39:36 +0700

    fix NaturalPerson class

---
+++ src/Cemetery/Registrar/Domain/NaturalPerson/NaturalPerson.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\NaturalPerson;
+
+use Cemetery\Registrar\Domain\AbstractAggregateRoot;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class NaturalPerson extends AbstractAggregateRoot
+{
+    /**
+     * @param NaturalPersonId         $id
+     * @param FullName|null           $fullName
+     * @param \DateTimeImmutable|null $bornAt
+     */
+    public function __construct(
+        private NaturalPersonId     $id,
+        private ?FullName           $fullName,
+        private ?\DateTimeImmutable $bornAt,
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @return NaturalPersonId
+     */
+    public function getId(): NaturalPersonId
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return FullName|null
+     */
+    public function getFullName(): ?FullName
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * @param FullName|null $fullName
+     */
+    public function setFullName(?FullName $fullName): void
+    {
+        $this->fullName = $fullName;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getBornAt(): ?\DateTimeImmutable
+    {
+        return $this->bornAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable|null $bornAt
+     */
+    public function setBornAt(?\DateTimeImmutable $bornAt): void
+    {
+        $this->bornAt = $bornAt;
+    }
+}
--- tests/Cemetery/Registrar/Domain/NaturalPerson/NaturalPersonTest.php
+++ tests/Cemetery/Registrar/Domain/NaturalPerson/NaturalPersonTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Cemetery\Tests\Registrar\Domain\NaturalPerson;
 
+use Cemetery\Registrar\Domain\NaturalPerson\FullName;
+use Cemetery\Registrar\Domain\NaturalPerson\NaturalPerson;
+use Cemetery\Registrar\Domain\NaturalPerson\NaturalPersonId;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -11,5 +14,33 @@ use PHPUnit\Framework\TestCase;
  */
 class NaturalPersonTest extends TestCase
 {
+    public function testItSuccessfullyCreated(): void
+    {
+        $naturalPersonId = new NaturalPersonId('777');
+        $fullName        = new FullName('Ivanov Ivan Ivanovich');
+        $bornAt          = new \DateTimeImmutable('2000-01-01');
+        $naturalPerson   = new NaturalPerson(
+            $naturalPersonId,
+            $fullName,
+            $bornAt,
+        );
 
+        $this->assertInstanceOf(NaturalPersonId::class, $naturalPerson->getId());
+        $this->assertSame('777', (string) $naturalPerson->getId());
+        $this->assertInstanceOf(FullName::class, $naturalPerson->getFullName());
+        $this->assertSame('Ivanov Ivan Ivanovich', (string) $naturalPerson->getFullName());
+        $this->assertInstanceOf(\DateTimeImmutable::class, $naturalPerson->getBornAt());
+        $this->assertSame('2000-01-01', $naturalPerson->getBornAt()->format('Y-m-d'));
+    }
+
+    public function testItSuccessfullyCreatedWithoutOptionalFields(): void
+    {
+        $naturalPersonId = new NaturalPersonId('777');
+        $naturalPerson   = new NaturalPerson($naturalPersonId, null, null);
+
+        $this->assertInstanceOf(NaturalPersonId::class, $naturalPerson->getId());
+        $this->assertSame('777', (string) $naturalPerson->getId());
+        $this->assertNull($naturalPerson->getFullName());
+        $this->assertNull($naturalPerson->getBornAt());
+    }
 }
