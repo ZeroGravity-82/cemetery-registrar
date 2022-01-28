ccb6f7c14cfd3de25eacfbf041c237a0c9191a08
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-28 13:06:31 +0700

    add DeceasedCollection class

---
+++ src/Cemetery/Registrar/Domain/Deceased/DeceasedCollection.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Deceased;
+
+use Cemetery\Registrar\Domain\AbstractEntityCollection;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class DeceasedCollection extends AbstractEntityCollection
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSupportedEntityClass(): string
+    {
+        return Deceased::class;
+    }
+}
--- tests/Cemetery/Registrar/Domain/Deceased/DeceasedCollectionTest.php
+++ tests/Cemetery/Registrar/Domain/Deceased/DeceasedCollectionTest.php
@@ -2,31 +2,43 @@
 
 declare(strict_types=1);
 
-namespace Cemetery\Tests\Registrar\Domain\Burial;
+namespace Cemetery\Tests\Registrar\Domain\Deceased;
 
+use Cemetery\Registrar\Domain\Deceased\CauseOfDeath;
+use Cemetery\Registrar\Domain\Deceased\DeathCertificateId;
 use Cemetery\Registrar\Domain\Deceased\Deceased;
 use Cemetery\Registrar\Domain\Deceased\DeceasedCollection;
 use Cemetery\Registrar\Domain\Deceased\DeceasedId;
+use Cemetery\Registrar\Domain\NaturalPerson\NaturalPersonId;
 use Cemetery\Tests\Registrar\Domain\AbstractEntityCollectionTest;
 
 /**
  * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
  */
-class BurialCollectionTest extends AbstractEntityCollectionTest
+class DeceasedCollectionTest extends AbstractEntityCollectionTest
 {
     public function setUp(): void
     {
-        $this->idA          = new DeceasedId('D001');
-        $this->idB          = new DeceasedId('D002');
-        $this->idC          = new DeceasedId('D003');
-        $this->idD          = new DeceasedId('D004');
-
-
-        $this->entityA      = new Deceased($this->idA, $burialCodeA, $naturalPersonIdA, $customerId, $siteIdA, null, null, null);
-        $this->entityB      = new Deceased($this->idB, $burialCodeB, $naturalPersonIdB, $customerId, $siteIdB, $naturalPersonIdB, null, $burialContainerIdB);
-        $this->entityC      = new Deceased($this->idC, $burialCodeC, $naturalPersonIdC, $customerId, $siteIdC, $naturalPersonIdC, $funeralCompanyIdC, $burialContainerIdC);
-        $this->entityD      = new Deceased($this->idD, $burialCodeD, $naturalPersonIdD, $customerId, null, null, null, null);
-        $this->collection   = new DeceasedCollection([$this->entityA]);
+        $this->idA           = new DeceasedId('D001');
+        $this->idB           = new DeceasedId('D002');
+        $this->idC           = new DeceasedId('D003');
+        $this->idD           = new DeceasedId('D004');
+        $naturalPersonIdA    = new NaturalPersonId('NP001');
+        $naturalPersonIdB    = new NaturalPersonId('NP002');
+        $naturalPersonIdC    = new NaturalPersonId('NP003');
+        $naturalPersonIdD    = new NaturalPersonId('NP004');
+        $diedAtA             = new \DateTimeImmutable('2021-12-01');
+        $diedAtB             = new \DateTimeImmutable('2001-02-11');
+        $diedAtC             = new \DateTimeImmutable('2011-05-13');
+        $diedAtD             = new \DateTimeImmutable('2015-03-10');
+        $deathCertificateIdB = new DeathCertificateId('DC001');
+        $deathCertificateIdC = new DeathCertificateId('DC002');
+        $causeOfDeathB       = new CauseOfDeath('Some cause 1');
+        $this->entityA       = new Deceased($this->idA, $naturalPersonIdA, $diedAtA, null, null);
+        $this->entityB       = new Deceased($this->idB, $naturalPersonIdB, $diedAtB, $deathCertificateIdB, $causeOfDeathB);
+        $this->entityC       = new Deceased($this->idC, $naturalPersonIdC, $diedAtC, $deathCertificateIdC, null);
+        $this->entityD       = new Deceased($this->idD, $naturalPersonIdD, $diedAtD, null, null);
+        $this->collection    = new DeceasedCollection([$this->entityA]);
     }
 
     public function testItReturnsEntityClass(): void
@@ -36,8 +48,8 @@ class BurialCollectionTest extends AbstractEntityCollectionTest
 
     protected function getClosureForCollectionFiltering(): \Closure
     {
-        return function (Deceased $burial) {
-            return $burial->getBurialPlaceOwnerId() !== null;
+        return function (Deceased $deceased) {
+            return $deceased->getDeathCertificateId() !== null;
         };
     }
 }
