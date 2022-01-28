921a7b816b55df2bb38ea09ed5b0499c433a6f18
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-01-28 12:27:35 +0700

    add DeceasedId value object

---
+++ src/Cemetery/Registrar/Domain/Deceased/DeceasedId.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Deceased;
+
+use Cemetery\Registrar\Domain\AbstractEntityId;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class DeceasedId extends AbstractEntityId
+{
+}
