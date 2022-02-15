844ac46ec972f345343be92737389590a4ff1866
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-02-15 13:21:35 +0700

    add identity value object for sole proprietor

---
+++ src/Cemetery/Registrar/Domain/Organization/SoleProprietor/SoleProprietorId.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Organization\SoleProprietor;
+
+use Cemetery\Registrar\Domain\AbstractEntityId;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class SoleProprietorId extends AbstractEntityId
+{
+}
---
+++ tests/Cemetery/Registrar/Domain/Organization/SoleProprietor/SoleProprietorIdTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Organization\SoleProprietor;
+
+use Cemetery\Registrar\Domain\Organization\SoleProprietor\SoleProprietorId;
+use Cemetery\Tests\Registrar\Domain\AbstractEntityIdTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class SoleProprietorIdTest extends AbstractEntityIdTest
+{
+    protected string $className = SoleProprietorId::class;
+}
