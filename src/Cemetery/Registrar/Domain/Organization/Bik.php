7bcd6b506da8e60fe9274e77fd6bfc14d8e980f4
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-02-19 01:51:20 +0700

    add bik value object

---
+++ src/Cemetery/Registrar/Domain/Organization/Bik.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Organization;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class Bik
+{
+    private const BIK_LENGTH = 9;
+
+    /**
+     * @param string $value
+     */
+    public function __construct(
+        private string $value,
+    ) {
+        $this->assertValidValue($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->getValue();
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param self $bik
+     *
+     * @return bool
+     */
+    public function isEqual(self $bik): bool
+    {
+        return $bik->getValue() === $this->getValue();
+    }
+
+    /**
+     * @param string $value
+     */
+    private function assertValidValue(string $value): void
+    {
+        $this->assertNotEmpty($value);
+        $this->assertNumeric($value);
+        $this->assertValidLength($value);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @throws \InvalidArgumentException when the BIK is empty
+     */
+    private function assertNotEmpty(string $value): void
+    {
+        if ($value === '') {
+            throw new \InvalidArgumentException('БИК не может иметь пустое значение.');
+        }
+    }
+
+    /**
+     * @param string $value
+     *
+     * @throws \InvalidArgumentException when the BIK has non-numeric value
+     */
+    private function assertNumeric(string $value): void
+    {
+        if (!\is_numeric($value)) {
+            throw new \InvalidArgumentException('БИК должен состоять только из цифр.');
+        }
+    }
+
+    /**
+     * @param string $value
+     *
+     * @throws \InvalidArgumentException when the length of the BIK is wrong
+     */
+    private function assertValidLength(string $value): void
+    {
+        $bikLength = self::BIK_LENGTH;
+        if (\strlen($value) !== $bikLength) {
+            throw new \InvalidArgumentException(\sprintf('БИК должен состоять из %d цифр.', $bikLength));
+        }
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/Organization/BikTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Organization;
+
+use Cemetery\Registrar\Domain\Organization\Bik;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class BikTest extends TestCase
+{
+    public function testItSuccessfullyCreated(): void
+    {
+        $bik = new Bik('045004774');
+        $this->assertSame($bik->getValue(), '045004774');
+    }
+
+    public function testItFailsWithEmptyValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('БИК не может иметь пустое значение.');
+        new Bik('');
+    }
+
+    public function testItFailsWithNonNumericValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('БИК должен состоять только из цифр.');
+        new Bik('04500477A');
+    }
+
+    public function testItFailsWithTooShortValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('БИК должен состоять из 9 цифр.');
+        new Bik('04500477');
+    }
+
+    public function testItFailsWithTooLongValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('БИК должен состоять из 9 цифр.');
+        new Bik('0450047741');
+    }
+
+    public function testItStringifyable(): void
+    {
+        $bik = new Bik('045004774');
+        $this->assertSame('045004774', (string) $bik);
+    }
+
+    public function testItComparable(): void
+    {
+        $bikA = new Bik('045004774');
+        $bikB = new Bik('044525974');
+        $bikC = new Bik('045004774');
+
+        $this->assertFalse($bikA->isEqual($bikB));
+        $this->assertTrue($bikA->isEqual($bikC));
+        $this->assertFalse($bikB->isEqual($bikC));
+    }
+}
