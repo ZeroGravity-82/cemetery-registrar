93aac59ef161ab1fb90ed23c584e2c0ff3c8da62
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-02-19 15:40:47 +0700

    fix correspondent account to use bik for validation

--- src/Cemetery/Registrar/Domain/Organization/BankDetails/CorrespondentAccount.php
+++ src/Cemetery/Registrar/Domain/Organization/BankDetails/CorrespondentAccount.php
@@ -13,11 +13,13 @@ final class CorrespondentAccount
 
     /**
      * @param string $value
+     * @param Bik    $bik
      */
     public function __construct(
         private string $value,
+        private Bik    $bik,
     ) {
-        $this->assertValidValue($value);
+        $this->assertValidValue($value, $bik);
     }
 
     /**
@@ -37,23 +39,36 @@ final class CorrespondentAccount
     }
 
     /**
+     * @return Bik
+     */
+    public function getBik(): Bik
+    {
+        return $this->bik;
+    }
+
+    /**
      * @param self $correspondentAccount
      *
      * @return bool
      */
     public function isEqual(self $correspondentAccount): bool
     {
-        return $correspondentAccount->getValue() === $this->getValue();
+        $isSameValue = $correspondentAccount->getValue() === $this->getValue();
+        $isSameBik   = $correspondentAccount->getBik()->isEqual($this->getBik());
+
+        return $isSameValue && $isSameBik;
     }
 
     /**
      * @param string $value
+     * @param Bik    $bik
      */
-    private function assertValidValue(string $value): void
+    private function assertValidValue(string $value, Bik $bik): void
     {
         $this->assertNotEmpty($value);
         $this->assertNumeric($value);
         $this->assertValidLength($value);
+        $this->assertValidCheckDigit($value, $bik);
     }
 
     /**
@@ -92,5 +107,38 @@ final class CorrespondentAccount
                 \sprintf('К/счёт должен состоять из %d цифр.', self::CORR_ACCOUNT_LENGTH)
             );
         }
+    }
+
+    /**
+     * @param string $value
+     * @param Bik    $bik
+     *
+     * @throws \InvalidArgumentException when the correspondent account contains an incorrect check digit
+     */
+    private function assertValidCheckDigit(string $value, Bik $bik): void
+    {
+        $checkDigit        = $this->calculateCheckDigit($value, $bik);
+        $isCheckDigitValid = $checkDigit === 0;
+        if (!$isCheckDigitValid) {
+            throw new \InvalidArgumentException('К/счёт недействителен (не соответствует БИК).');
+        }
+    }
+
+    /**
+     * @param string $value
+     * @param Bik    $bik
+     *
+     * @return int
+     */
+    private function calculateCheckDigit(string $value, Bik $bik): int
+    {
+        $checkSum     = 0;
+        $coefficients = [7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1];
+        $stringToTest = '0' . \substr($bik->getValue(), -5, 2) . $value;
+        foreach ($coefficients as $index => $coefficient) {
+            $checkSum += $coefficient * ((int) $stringToTest[$index] % 10);
+        }
+
+        return $checkSum % 10;
     }
 }
--- tests/Cemetery/Registrar/Domain/Organization/BankDetails/CorrespondentAccountTest.php
+++ tests/Cemetery/Registrar/Domain/Organization/BankDetails/CorrespondentAccountTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Cemetery\Tests\Registrar\Domain\Organization\BankDetails;
 
+use Cemetery\Registrar\Domain\Organization\BankDetails\Bik;
 use Cemetery\Registrar\Domain\Organization\BankDetails\CorrespondentAccount;
 use PHPUnit\Framework\TestCase;
 
@@ -14,49 +15,59 @@ class CorrespondentAccountTest extends TestCase
 {
     public function testItSuccessfullyCreated(): void
     {
-        $correspondentAccount = new CorrespondentAccount('30101810600000000774');
+        // Correspondent account opened with a credit institution
+        $correspondentAccount = new CorrespondentAccount('30101810600000000774', new Bik('045004774'));
         $this->assertSame($correspondentAccount->getValue(), '30101810600000000774');
+        $this->assertInstanceOf(Bik::class, $correspondentAccount->getBik());
+        $this->assertSame('045004774', (string) $correspondentAccount->getBik());
     }
 
     public function testItFailsWithEmptyValue(): void
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('К/счёт не может иметь пустое значение.');
-        new CorrespondentAccount('');
+        new CorrespondentAccount('', new Bik('045004774'));
     }
 
     public function testItFailsWithNonNumericValue(): void
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('К/счёт должен состоять только из цифр.');
-        new CorrespondentAccount('3010181060000000077A');
+        new CorrespondentAccount('3010181060000000077A', new Bik('045004774'));
     }
 
     public function testItFailsWithTooShortValue(): void
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('К/счёт должен состоять из 20 цифр.');
-        new CorrespondentAccount('3010181060000000077');
+        new CorrespondentAccount('3010181060000000077', new Bik('045004774'));
     }
 
     public function testItFailsWithTooLongValue(): void
     {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('К/счёт должен состоять из 20 цифр.');
-        new CorrespondentAccount('301018106000000007741');
+        new CorrespondentAccount('301018106000000007741', new Bik('045004774'));
+    }
+
+    public function testItFailsWithWrongBikValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('К/счёт недействителен (не соответствует БИК).');
+        new CorrespondentAccount('30101810600000000774', new Bik('044525974'));
     }
 
     public function testItStringifyable(): void
     {
-        $correspondentAccount = new CorrespondentAccount('30101810600000000774');
+        $correspondentAccount = new CorrespondentAccount('30101810600000000774', new Bik('045004774'));
         $this->assertSame('30101810600000000774', (string) $correspondentAccount);
     }
 
     public function testItComparable(): void
     {
-        $correspondentAccountA = new CorrespondentAccount('30101810600000000774');
-        $correspondentAccountB = new CorrespondentAccount('30101810145250000974');
-        $correspondentAccountC = new CorrespondentAccount('30101810600000000774');
+        $correspondentAccountA = new CorrespondentAccount('30101810600000000774', new Bik('045004774'));
+        $correspondentAccountB = new CorrespondentAccount('30101810145250000974', new Bik('044525974'));
+        $correspondentAccountC = new CorrespondentAccount('30101810600000000774', new Bik('045004774'));
 
         $this->assertFalse($correspondentAccountA->isEqual($correspondentAccountB));
         $this->assertTrue($correspondentAccountA->isEqual($correspondentAccountC));
