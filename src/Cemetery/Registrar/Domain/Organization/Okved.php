7a50609508fcef216652b327574268af469caad9
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-02-21 10:03:38 +0700

    [wip] add okved value object

---
+++ src/Cemetery/Registrar/Domain/Organization/Okved.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Organization;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class Okved
+{
+    /**
+     * @param string $value
+     */
+    public function __construct(
+        private string $value,
+    ) {
+        $this->assertValidValue($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->getValue();
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param self $okved
+     *
+     * @return bool
+     */
+    public function isEqual(self $okved): bool
+    {
+        return $okved->getValue() === $this->getValue();
+    }
+
+    /**
+     * @param string $value
+     */
+    private function assertValidValue(string $value): void
+    {
+        $this->assertNotEmpty($value);
+        $this->assertValidFormat($value);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @throws \InvalidArgumentException when the OKVED is empty
+     */
+    private function assertNotEmpty(string $value): void
+    {
+        if ($value === '') {
+            throw new \InvalidArgumentException('ОКВЭД не может иметь пустое значение.');
+        }
+    }
+
+    /**
+     * @param string $value
+     *
+     * @throws \InvalidArgumentException when the OKVED invalid format
+     */
+    private function assertValidFormat(string $value): void
+    {
+        if (!\preg_match('~[\d+\.]+\d+~')) {
+            throw new \InvalidArgumentException('ОКВЭД должен состоять только из цифр.');
+        }
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/Organization/OkvedTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Organization;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class OkvedTest extends TestCase
+{
+    public function testItSuccessfullyCreated(): void
+    {
+        $okved = new Okved('74.82');
+        $this->assertSame($okved->getValue(), '74.82');
+    }
+
+    public function testItFailsWithEmptyValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('ОКВЭД не может иметь пустое значение.');
+        new Okved('');
+    }
+
+    public function testItFailsWithInvalidFormatA(): void
+    {
+        $this->expectExceptionForInvalidFormat();
+        new Okved('7482.');
+    }
+
+    public function testItFailsWithInvalidFormatB(): void
+    {
+        $this->expectExceptionForInvalidFormat();
+        new Okved('74.8A');
+    }
+
+    public function testItStringifyable(): void
+    {
+        $okved = new Okved('74.82');
+        $this->assertSame('74.82', (string) $okved);
+    }
+
+    public function testItComparable(): void
+    {
+        $okvedA = new Okved('74.82');
+        $okvedB = new Okved('90.01');
+        $okvedC = new Okved('74.82');
+
+        $this->assertFalse($okvedA->isEqual($okvedB));
+        $this->assertTrue($okvedA->isEqual($okvedC));
+        $this->assertFalse($okvedB->isEqual($okvedC));
+    }
+
+    private function expectExceptionForInvalidFormat(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('ОКВЭД должен состоять из чисел, разделённых точками.');
+    }
+}
