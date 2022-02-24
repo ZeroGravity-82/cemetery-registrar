aab9a609aede499713931cd344cca8b2a428bd59
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-02-24 19:45:46 +0700

    add address value object

---
+++ src/Cemetery/Registrar/Domain/Contact/Address.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Contact;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class Address
+{
+    /**
+     * @param string $value
+     */
+    public function __construct(
+        private string $value,
+    ) {
+        $this->assertValidValue($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->getValue();
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param self $address
+     *
+     * @return bool
+     */
+    public function isEqual(self $address): bool
+    {
+        return $address->getValue() === $this->getValue();
+    }
+
+    /**
+     * @param string $value
+     */
+    private function assertValidValue(string $value): void
+    {
+        $this->assertNotEmpty($value);
+    }
+
+    /**
+     * @param string $value
+     *
+     * @throws \InvalidArgumentException when the address is empty
+     */
+    private function assertNotEmpty(string $value): void
+    {
+        if ($value === '') {
+            throw new \InvalidArgumentException('Адрес не может иметь пустое значение.');
+        }
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/Contact/AddressTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Contact;
+
+use Cemetery\Registrar\Domain\Contact\Address;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class AddressTest extends TestCase
+{
+        public function testItSuccessfullyCreated(): void
+    {
+        $address = new Address('г. Новосибирск, ул. 3 Интернационала, д. 127');
+        $this->assertSame($address->getValue(), 'г. Новосибирск, ул. 3 Интернационала, д. 127');
+    }
+
+    public function testItFailsWithEmptyValue(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Адрес не может иметь пустое значение.');
+        new Address('');
+    }
+
+    public function testItStringifyable(): void
+    {
+        $coordinates = new Address('г. Новосибирск, ул. 3 Интернационала, д. 127');
+        $this->assertSame('г. Новосибирск, ул. 3 Интернационала, д. 127', (string) $coordinates);
+    }
+
+    public function testItComparable(): void
+    {
+        $addressA = new Address('г. Новосибирск, ул. 3 Интернационала, д. 127');
+        $addressB = new Address('г. Москва, ул. Стандартная, д. 21, корп. 1, кв. 5');
+        $addressC = new Address('г. Новосибирск, ул. 3 Интернационала, д. 127');
+
+        $this->assertFalse($addressA->isEqual($addressB));
+        $this->assertTrue($addressA->isEqual($addressC));
+        $this->assertFalse($addressB->isEqual($addressC));
+    }
+}
