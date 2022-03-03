9c3e47e8d847db5022e8f0d3f376b08bb436021a
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-03-04 00:51:57 +0700

    add factory class for the juristic person entity

---
+++ src/Cemetery/Registrar/Domain/Organization/JuristicPerson/JuristicPersonFactory.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Organization\JuristicPerson;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class JuristicPersonFactory
+{
+    /**
+     * @param JuristicPersonBuilder $builder
+     */
+    public function __construct(
+        private JuristicPersonBuilder $builder,
+    ) {}
+
+    /**
+     * @param string|null $name
+     * @param string|null $inn
+     * @param string|null $kpp
+     * @param string|null $ogrn
+     * @param string|null $okpo
+     * @param string|null $okved
+     * @param string|null $legalAddress
+     * @param string|null $postalAddress
+     * @param string|null $bankDetailsBankName
+     * @param string|null $bankDetailsBik
+     * @param string|null $bankDetailsCorrespondentAccount
+     * @param string|null $bankDetailsCurrentAccount
+     * @param string|null $phone
+     * @param string|null $phoneAdditional
+     * @param string|null $fax
+     * @param string|null $email
+     * @param string|null $website
+     *
+     * @return JuristicPerson
+     */
+    public function createJuristicPersonForCustomer(
+        ?string $name,
+        ?string $inn,
+        ?string $kpp,
+        ?string $ogrn,
+        ?string $okpo,
+        ?string $okved,
+        ?string $legalAddress,
+        ?string $postalAddress,
+        ?string $bankDetailsBankName,
+        ?string $bankDetailsBik,
+        ?string $bankDetailsCorrespondentAccount,
+        ?string $bankDetailsCurrentAccount,
+        ?string $phone,
+        ?string $phoneAdditional,
+        ?string $fax,
+        ?string $generalDirector,
+        ?string $email,
+        ?string $website,
+    ): JuristicPerson {
+        $this->assertNameIsProvided($name);
+        $this->builder->initialize($name);
+
+        if ($inn !== null) {
+            $this->builder->addInn($inn);
+        }
+        if ($kpp !== null) {
+            $this->builder->addKpp($kpp);
+        }
+        if ($ogrn !== null) {
+            $this->builder->addOgrn($ogrn);
+        }
+        if ($okpo !== null) {
+            $this->builder->addOkpo($okpo);
+        }
+        if ($okved !== null) {
+            $this->builder->addOkved($okved);
+        }
+        if ($legalAddress !== null) {
+            $this->builder->addLegalAddress($legalAddress);
+        }
+        if ($postalAddress !== null) {
+            $this->builder->addPostalAddress($postalAddress);
+        }
+        if (
+            $bankDetailsBankName !== null &&
+            $bankDetailsBik !== null &&
+            $bankDetailsCurrentAccount !== null
+        ) {
+            $this->builder->addBankDetails(
+                $bankDetailsBankName,
+                $bankDetailsBik,
+                $bankDetailsCorrespondentAccount,
+                $bankDetailsCurrentAccount,
+            );
+        }
+        if ($phone !== null) {
+            $this->builder->addPhone($phone);
+        }
+        if ($phoneAdditional !== null) {
+            $this->builder->addPhoneAdditional($phoneAdditional);
+        }
+        if ($fax !== null) {
+            $this->builder->addFax($fax);
+        }
+        if ($generalDirector !== null) {
+            $this->builder->addGeneralDirector($generalDirector);
+        }
+        if ($email !== null) {
+            $this->builder->addEmail($email);
+        }
+        if ($website !== null) {
+            $this->builder->addWebsite($website);
+        }
+
+        return $this->builder->build();
+    }
+
+    /**
+     * @param string|null $name
+     *
+     * @throws \RuntimeException when the name is not provided
+     */
+    private function assertNameIsProvided(?string $name): void
+    {
+        if ($name === null) {
+            throw new \RuntimeException('Наименование не указано.');
+        }
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/Organization/JuristicPerson/JuristicPersonFactoryTest.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Organization\JuristicPerson;
+
+use Cemetery\Registrar\Domain\Organization\JuristicPerson\JuristicPerson;
+use Cemetery\Registrar\Domain\Organization\JuristicPerson\JuristicPersonBuilder;
+use Cemetery\Registrar\Domain\Organization\JuristicPerson\JuristicPersonFactory;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class JuristicPersonFactoryTest extends TestCase
+{
+    private MockObject|JuristicPersonBuilder $mockJuristicPersonBuilder;
+    private JuristicPersonFactory            $juristicPersonFactory;
+    private MockObject|JuristicPerson        $mockJuristicPerson;
+
+    public function setUp(): void
+    {
+        $this->mockJuristicPersonBuilder = $this->createMock(JuristicPersonBuilder::class);
+        $this->juristicPersonFactory     = new JuristicPersonFactory($this->mockJuristicPersonBuilder);
+        $this->mockJuristicPerson        = $this->createMock(JuristicPerson::class);
+    }
+
+    public function testItCreatesJuristicPersonForCustomer(): void
+    {
+        $name                            = 'ООО "Рога и копыта"';
+        $inn                             = '7728168971';
+        $kpp                             = '1234AB789';
+        $ogrn                            = '1027700132195';
+        $okpo                            = '23584736';
+        $okved                           = '74.82';
+        $legalAddress                    = 'г. Новосибирск, ул. 3 Интернационала, д. 127';
+        $postalAddress                   = 'г. Москва, ул. Стандартная, д. 21, корп. 1, кв. 5';
+        $bankDetailsBankName             = 'Сибирский филиал Публичного акционерного общества "Промсвязьбанк"';
+        $bankDetailsBik                  = '045004816';
+        $bankDetailsCorrespondentAccount = '30101810500000000816';
+        $bankDetailsCurrentAccount       = '40702810904000040651';
+        $phone                           = '+7-913-777-88-99';
+        $phoneAdditional                 = '8(383)123-45-67';
+        $fax                             = '8(383)123-46-89';
+        $generalDirector                 = 'Иванов Иван Иванович';
+        $email                           = 'info@google.com';
+        $website                         = 'https://example.com';
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('initialize')->with($name);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addInn')->with($inn);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addKpp')->with($kpp);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addOgrn')->with($ogrn);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addOkpo')->with($okpo);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addOkved')->with($okved);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addLegalAddress')->with($legalAddress);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addPostalAddress')->with($postalAddress);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addBankDetails')->with(
+            $bankDetailsBankName,
+            $bankDetailsBik,
+            $bankDetailsCorrespondentAccount,
+            $bankDetailsCurrentAccount,
+        );
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addPhone')->with($phone);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addPhoneAdditional')->with($phoneAdditional);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addFax')->with($fax);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addGeneralDirector')->with($generalDirector);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addEmail')->with($email);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('addWebsite')->with($website);
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('build')->willReturn($this->mockJuristicPerson);
+        $juristicPerson = $this->juristicPersonFactory->createJuristicPersonForCustomer(
+            $name,
+            $inn,
+            $kpp,
+            $ogrn,
+            $okpo,
+            $okved,
+            $legalAddress,
+            $postalAddress,
+            $bankDetailsBankName,
+            $bankDetailsBik,
+            $bankDetailsCorrespondentAccount,
+            $bankDetailsCurrentAccount,
+            $phone,
+            $phoneAdditional,
+            $fax,
+            $generalDirector,
+            $email,
+            $website,
+        );
+        $this->assertInstanceOf(JuristicPerson::class, $juristicPerson);
+    }
+
+    public function testItCreatesJuristicPersonForCustomerWithoutOptionalFields(): void
+    {
+        $name = 'ООО "Рога и копыта"';
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('initialize')->with($name);
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addInn');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addKpp');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addOgrn');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addOkpo');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addOkved');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addLegalAddress');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addPostalAddress');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addBankDetails');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addPhone');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addPhoneAdditional');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addFax');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addGeneralDirector');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addEmail');
+        $this->mockJuristicPersonBuilder->expects($this->never())->method('addWebsite');
+        $this->mockJuristicPersonBuilder->expects($this->once())->method('build')->willReturn($this->mockJuristicPerson);
+        $juristicPerson = $this->juristicPersonFactory->createJuristicPersonForCustomer(
+            $name,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+        );
+        $this->assertInstanceOf(JuristicPerson::class, $juristicPerson);
+    }
+
+    public function testItFailsToCreateJuristicPersonForCustomerWithoutName(): void
+    {
+        $this->expectExceptionForNotProvidedName();
+        $this->juristicPersonFactory->createJuristicPersonForCustomer(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+        );
+    }
+
+    private function expectExceptionForNotProvidedName(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Наименование не указано.');
+    }
+}
