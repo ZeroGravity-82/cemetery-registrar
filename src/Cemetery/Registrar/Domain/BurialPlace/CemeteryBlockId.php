dbd5b41ac49b8158cb8836408d82ac01007f49c2
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-04-22 16:05:46 +0700

    add value object for cemetery block

---
+++ src/Cemetery/Registrar/Domain/BurialPlace/CemeteryBlockId.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\BurialPlace;
+
+use Cemetery\Registrar\Domain\EntityId;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class CemeteryBlockId extends EntityId
+{
+}
