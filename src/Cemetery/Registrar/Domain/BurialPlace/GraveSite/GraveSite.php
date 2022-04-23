e4aa117ba4986df5c8c0a6c329c4f266984a6e6d
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-04-23 20:25:22 +0700

    add grave site entity

---
+++ src/Cemetery/Registrar/Domain/BurialPlace/GraveSite/GraveSite.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\AggregateRoot;
+use Cemetery\Registrar\Domain\BurialPlace\CemeteryBlockId;
+use Cemetery\Registrar\Domain\GeoPosition\GeoPosition;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class GraveSite extends AggregateRoot
+{
+    public const LABEL = 'участок на кладбище';
+
+    /**
+     * @var PositionInRow|null
+     */
+    private ?PositionInRow $positionInRow = null;
+
+    /**
+     * @var GeoPosition|null
+     */
+    private ?GeoPosition $geoPosition = null;
+
+    /**
+     * @var GraveSiteSize|null
+     */
+    private ?GraveSiteSize $size = null;
+
+    /**
+     * @param GraveSiteId     $id
+     * @param CemeteryBlockId $cemeteryBlockId
+     * @param RowInBlock      $rowInBlock
+     */
+    public function __construct(
+        private readonly GraveSiteId $id,
+        private CemeteryBlockId      $cemeteryBlockId,
+        private RowInBlock           $rowInBlock,
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @return GraveSiteId
+     */
+    public function id(): GraveSiteId
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return CemeteryBlockId
+     */
+    public function cemeteryBlockId(): CemeteryBlockId
+    {
+        return $this->cemeteryBlockId;
+    }
+
+    /**
+     * @param CemeteryBlockId $cemeteryBlockId
+     *
+     * @return $this
+     */
+    public function setCemeteryBlockId(CemeteryBlockId $cemeteryBlockId): self
+    {
+        $this->cemeteryBlockId = $cemeteryBlockId;
+
+        return $this;
+    }
+
+    /**
+     * @return RowInBlock
+     */
+    public function rowInBlock(): RowInBlock
+    {
+        return $this->rowInBlock;
+    }
+
+    /**
+     * @param RowInBlock $rowInBlock
+     *
+     * @return $this
+     */
+    public function setRowInBlock(RowInBlock $rowInBlock): self
+    {
+        $this->rowInBlock = $rowInBlock;
+
+        return $this;
+    }
+
+    /**
+     * @return PositionInRow|null
+     */
+    public function positionInRow(): ?PositionInRow
+    {
+        return $this->positionInRow;
+    }
+
+    /**
+     * @param PositionInRow|null $positionInRow
+     *
+     * @return $this
+     */
+    public function setPositionInRow(?PositionInRow $positionInRow): self
+    {
+        $this->positionInRow = $positionInRow;
+
+        return $this;
+    }
+
+    /**
+     * @return GeoPosition|null
+     */
+    public function geoPosition(): ?GeoPosition
+    {
+        return $this->geoPosition;
+    }
+
+    /**
+     * @param GeoPosition|null $geoPosition
+     *
+     * @return $this
+     */
+    public function setGeoPosition(?GeoPosition $geoPosition): self
+    {
+        $this->geoPosition = $geoPosition;
+
+        return $this;
+    }
+
+    /**
+     * @return GraveSiteSize|null
+     */
+    public function size(): ?GraveSiteSize
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param GraveSiteSize|null $size
+     *
+     * @return $this
+     */
+    public function setSize(?GraveSiteSize $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+}
--- tests/Cemetery/Registrar/Domain/BurialPlace/GraveSite/GraveSiteTest.php
+++ tests/Cemetery/Registrar/Domain/BurialPlace/GraveSite/GraveSiteTest.php
@@ -8,35 +8,79 @@ use Cemetery\Registrar\Domain\BurialPlace\CemeteryBlockId;
 use Cemetery\Registrar\Domain\BurialPlace\GraveSite\GraveSite;
 use Cemetery\Registrar\Domain\BurialPlace\GraveSite\GraveSiteId;
 use Cemetery\Registrar\Domain\BurialPlace\GraveSite\GraveSiteSize;
+use Cemetery\Registrar\Domain\BurialPlace\GraveSite\PositionInRow;
 use Cemetery\Registrar\Domain\BurialPlace\GraveSite\RowInBlock;
-use Cemetery\Registrar\Domain\Geolocation\Accuracy;
-use Cemetery\Registrar\Domain\Geolocation\Coordinates;
-use Cemetery\Registrar\Domain\Geolocation\Position;
-use PHPUnit\Framework\TestCase;
+use Cemetery\Registrar\Domain\GeoPosition\Accuracy;
+use Cemetery\Registrar\Domain\GeoPosition\Coordinates;
+use Cemetery\Registrar\Domain\GeoPosition\GeoPosition;
+use Cemetery\Tests\Registrar\Domain\AbstractAggregateRootTest;
 
 /**
  * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
  */
-class GraveSiteTest extends TestCase
+class GraveSiteTest extends AbstractAggregateRootTest
 {
+    private GraveSite $graveSite;
+
+    public function setUp(): void
+    {
+        $id               = new GraveSiteId('GS001');
+        $cemeteryBlockId  = new CemeteryBlockId('CB001');
+        $rowInBlock       = new RowInBlock(5);
+        $this->graveSite  = new GraveSite($id, $cemeteryBlockId, $rowInBlock);
+        $this->entity     = $this->graveSite;
+    }
+
     public function testItHasValidLabelConstant(): void
     {
-        $this->assertSame('могила', GraveSite::LABEL);
+        $this->assertSame('участок на кладбище', GraveSite::LABEL);
     }
 
     public function testItSuccessfullyCreated(): void
     {
-        $id              = new GraveSiteId('GS001');
-        $cemeteryBlockId = new CemeteryBlockId('CB001');
-        $rowInBlock      = new RowInBlock('RIB001');
-        $position        = new Position(new Coordinates('54.9472658', '82.8043771'), new Accuracy('0.25'));
-        $size            = new GraveSiteSize('2.5');
-        $graveSite       = new GraveSite($id, $cemeteryBlockId, $rowInBlock, $position, $size);
+        $this->assertInstanceOf(GraveSiteId::class, $this->graveSite->id());
+        $this->assertSame('GS001', $this->graveSite->id()->value());
+        $this->assertInstanceOf(CemeteryBlockId::class, $this->graveSite->cemeteryBlockId());
+        $this->assertSame('CB001', $this->graveSite->cemeteryBlockId()->value());
+        $this->assertInstanceOf(RowInBlock::class, $this->graveSite->rowInBlock());
+        $this->assertSame(5, $this->graveSite->rowInBlock()->value());
+        $this->assertNull($this->graveSite->positionInRow());
+        $this->assertNull($this->graveSite->geoPosition());
+        $this->assertNull($this->graveSite->size());
     }
 
-    public function testItFailsWithEmptyValue(): void
+    public function testItSetsCemeteryBlockId(): void
     {
-
+        $cemeteryBlockId = new CemeteryBlockId('CB002');
+        $this->graveSite->setCemeteryBlockId($cemeteryBlockId);
+        $this->assertTrue($this->graveSite->cemeteryBlockId()->isEqual($cemeteryBlockId));
     }
 
+    public function testItSetsRowInBlock(): void
+    {
+        $rowInBlock = new RowInBlock(6);
+        $this->graveSite->setRowInBlock($rowInBlock);
+        $this->assertTrue($this->graveSite->rowInBlock()->isEqual($rowInBlock));
+    }
+
+    public function testItSetsPositionInRow(): void
+    {
+        $positionInRow = new PositionInRow(10);
+        $this->graveSite->setPositionInRow($positionInRow);
+        $this->assertTrue($this->graveSite->positionInRow()->isEqual($positionInRow));
+    }
+
+    public function testItSetsGeoPosition(): void
+    {
+        $geoPosition = new GeoPosition(new Coordinates('54.9472658', '82.8043771'), new Accuracy('0.25'));
+        $this->graveSite->setGeoPosition($geoPosition);
+        $this->assertTrue($this->graveSite->geoPosition()->isEqual($geoPosition));
+    }
+
+    public function testItSetsSize(): void
+    {
+        $size = new GraveSiteSize('2.5');
+        $this->graveSite->setSize($size);
+        $this->assertTrue($this->graveSite->size()->isEqual($size));
+    }
 }
