8711327b28ef8a8bfa695fe57d48f5a8dbd23b02
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-04-26 15:52:31 +0700

    add columbarium entity

---
+++ tests/Cemetery/Registrar/Domain/BurialPlace/ColumbariumNiche/ColumbariumTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\BurialPlace\ColumbariumNiche;
+
+use Cemetery\Registrar\Domain\BurialPlace\ColumbariumNiche\Columbarium;
+use Cemetery\Registrar\Domain\BurialPlace\ColumbariumNiche\ColumbariumId;
+use Cemetery\Registrar\Domain\BurialPlace\ColumbariumNiche\ColumbariumName;
+use Cemetery\Registrar\Domain\GeoPosition\Accuracy;
+use Cemetery\Registrar\Domain\GeoPosition\Coordinates;
+use Cemetery\Registrar\Domain\GeoPosition\GeoPosition;
+use Cemetery\Tests\Registrar\Domain\AbstractAggregateRootTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class ColumbariumTest extends AbstractAggregateRootTest
+{
+    private ColumbariumId   $id;
+    private ColumbariumName $name;
+    private Columbarium     $columbarium;
+
+    public function setUp(): void
+    {
+        $this->id          = new ColumbariumId('C001');
+        $this->name        = new ColumbariumName('западный колумбарий');
+        $this->columbarium = new Columbarium($this->id, $this->name);
+        $this->entity      = $this->columbarium;
+    }
+
+    public function testItSuccessfullyCreated(): void
+    {
+        $this->assertInstanceOf(ColumbariumId::class, $this->columbarium->id());
+        $this->assertTrue($this->columbarium->id()->isEqual($this->id));
+        $this->assertInstanceOf(ColumbariumName::class, $this->columbarium->name());
+        $this->assertTrue($this->columbarium->name()->isEqual($this->name));
+        $this->assertNull($this->columbarium->geoPosition());
+    }
+
+    public function testItSetsName(): void
+    {
+        $name = new ColumbariumName('восточный колумбарий');
+        $this->columbarium->setName($name);
+        $this->assertInstanceOf(ColumbariumName::class, $this->columbarium->name());
+        $this->assertTrue($this->columbarium->name()->isEqual($name));
+    }
+
+    public function testItSetsGeoPosition(): void
+    {
+        $geoPosition = new GeoPosition(new Coordinates('54.950357', '82.7972252'), new Accuracy('0.5'));
+        $this->columbarium->setGeoPosition($geoPosition);
+        $this->assertInstanceOf(GeoPosition::class, $this->columbarium->geoPosition());
+        $this->assertTrue($this->columbarium->geoPosition()->isEqual($geoPosition));
+    }
+}
