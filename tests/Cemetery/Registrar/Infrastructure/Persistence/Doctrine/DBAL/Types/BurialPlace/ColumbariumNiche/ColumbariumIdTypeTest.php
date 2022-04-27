84158ea14c3cc9088af15e79091c36921a8d5e31
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-04-27 13:25:49 +0700

    add Doctrine DBAL custom type for columbarium ID

---
+++ tests/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/DBAL/Types/BurialPlace/ColumbariumNiche/ColumbariumIdTypeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\BurialPlace\ColumbariumNiche;
+
+use Cemetery\Registrar\Domain\BurialPlace\ColumbariumNiche\ColumbariumId;
+use Cemetery\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\BurialPlace\ColumbariumNiche\ColumbariumIdType;
+use Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\StringTypeTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class ColumbariumIdTypeTest extends StringTypeTest
+{
+    protected string $className = ColumbariumIdType::class;
+
+    protected string $typeName = 'columbarium_id';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dbValue  = 'C001';
+        $this->phpValue = new ColumbariumId('C001');
+    }
+}
