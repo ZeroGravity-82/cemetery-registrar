56228c3f85c75b59dd0075155d47f8eae8e6f1e5
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-01 12:15:12 +0700

    add Doctrine DBAL custom type for geo position value object

---
+++ src/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/DBAL/Types/GeoPosition/GeoPositionType.php
@@ -0,0 +1,140 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\GeoPosition;
+
+use Cemetery\Registrar\Domain\GeoPosition\Accuracy;
+use Cemetery\Registrar\Domain\GeoPosition\Coordinates;
+use Cemetery\Registrar\Domain\GeoPosition\GeoPosition;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\JsonType;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class GeoPositionType extends JsonType
+{
+    private const TYPE_NAME = 'geo_position';
+
+    /**
+     * Registers type to the type map.
+     */
+    public static function registerType(): void
+    {
+        if (self::hasType(self::TYPE_NAME)) {
+            return;
+        }
+        self::addType(self::TYPE_NAME, self::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+    {
+        if ($value === null) {
+            return $value;
+        }
+
+        if (!$value instanceof GeoPosition) {
+            throw ConversionException::conversionFailedInvalidType(
+                $value,
+                $this->getName(),
+                ['null', GeoPosition::class]
+            );
+        }
+
+        try {
+            return \json_encode($this->prepareGeoPositionForEncoding($value), JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            throw ConversionException::conversionFailedSerialization($value, 'json', $e->getMessage(), $e);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?GeoPosition
+    {
+        if ($value === null || $value instanceof GeoPosition) {
+            return $value;
+        }
+
+        try {
+            $decodedValue = \json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+            $this->assertValid($decodedValue, $value);
+
+            return $this->buildGeoPosition($decodedValue);
+        } catch (\JsonException $e) {
+            throw ConversionException::conversionFailed($value, $this->getName(), $e);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return self::TYPE_NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param mixed $decodedValue
+     * @param mixed $value
+     *
+     * @throws \RuntimeException when the decoded value has invalid format.
+     */
+    private function assertValid(mixed $decodedValue, mixed $value): void
+    {
+        $isInvalidValue =
+            !\array_key_exists('coordinates', $decodedValue)                ||
+            !\array_key_exists('latitude',    $decodedValue['coordinates']) ||
+            !\array_key_exists('longitude',   $decodedValue['coordinates']) ||
+            !\array_key_exists('accuracy',    $decodedValue);
+        if ($isInvalidValue) {
+            throw new \RuntimeException(\sprintf('Неверный формат геопозиции: "%s".', $value));
+        }
+    }
+
+    /**
+     * @param GeoPosition $geoPosition
+     *
+     * @return array
+     */
+    private function prepareGeoPositionForEncoding(GeoPosition $geoPosition): array
+    {
+        return [
+            'coordinates' => [
+                'latitude'  => $geoPosition->coordinates()->latitude(),
+                'longitude' => $geoPosition->coordinates()->longitude(),
+            ],
+            'accuracy'    => $geoPosition->accuracy()?->value(),
+        ];
+    }
+
+    /**
+     * @param array $decodedValue
+     *
+     * @return GeoPosition
+     */
+    private function buildGeoPosition(array $decodedValue): GeoPosition
+    {
+        return new GeoPosition(
+            new Coordinates(
+                (string) $decodedValue['coordinates']['latitude'],
+                (string) $decodedValue['coordinates']['longitude']
+            ),
+            !\is_null($decodedValue['accuracy']) ? new Accuracy((string) $decodedValue['accuracy']) : null
+        );
+    }
+}
---
+++ tests/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/DBAL/Types/GeoPosition/GeoPositionTypeTest.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\GeoPosition;
+
+use Cemetery\Registrar\Domain\GeoPosition\Accuracy;
+use Cemetery\Registrar\Domain\GeoPosition\Coordinates;
+use Cemetery\Registrar\Domain\GeoPosition\GeoPosition;
+use Cemetery\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\GeoPosition\GeoPositionType;
+use Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\TypeTest;
+use Doctrine\DBAL\Types\ConversionException;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class GeoPositionTypeTest extends TypeTest
+{
+    protected string $className         = GeoPositionType::class;
+    protected string $typeName          = 'geo_position';
+    protected string $phpValueClassName = GeoPosition::class;
+
+    /**
+     * @dataProvider getConversionTests
+     */
+    public function testItConvertsToDatabaseValue(string $dbValue, GeoPosition $phpValue): void
+    {
+        $resultingDbValue     = $this->type->convertToDatabaseValue($phpValue, $this->mockPlatform);
+        $decodedResultDbValue = \json_decode($resultingDbValue, true);
+        $this->assertIsArray($decodedResultDbValue);
+        $this->assertArrayHasKey('coordinates', $decodedResultDbValue);
+        $this->assertArrayHasKey('accuracy', $decodedResultDbValue);
+        $this->assertIsArray($decodedResultDbValue['coordinates']);
+        $this->assertArrayHasKey('latitude', $decodedResultDbValue['coordinates']);
+        $this->assertArrayHasKey('longitude', $decodedResultDbValue['coordinates']);
+        $this->assertSame($phpValue->coordinates()->latitude(), (string) $decodedResultDbValue['coordinates']['latitude']);
+        $this->assertSame($phpValue->coordinates()->longitude(), (string) $decodedResultDbValue['coordinates']['longitude']);
+        $this->assertSame($phpValue->accuracy()?->value(), $decodedResultDbValue['accuracy']);
+    }
+
+    public function testItConvertsToDatabaseNullValue(): void
+    {
+        $resultingDbValue = $this->type->convertToDatabaseValue(null, $this->mockPlatform);
+        $this->assertNull($resultingDbValue);
+    }
+
+    public function testItFailsToConvertPhpValueOfInvalidTypeToDatabaseValue(): void
+    {
+        $valueOfInvalidType = new \stdClass();
+        $this->expectException(ConversionException::class);
+        $this->expectExceptionMessage(\sprintf(
+            'Could not convert PHP value of type %s to type %s. Expected one of the following types: null, %s',
+            \get_class($valueOfInvalidType),
+            $this->typeName,
+            $this->phpValueClassName,
+        ));
+        $this->type->convertToDatabaseValue($valueOfInvalidType, $this->mockPlatform);
+    }
+
+    /**
+     * @dataProvider getConversionTests
+     */
+    public function testItConvertsToPhpValue(string $dbValue, GeoPosition $phpValue): void
+    {
+        $resultingPhpValue = $this->type->convertToPHPValue($dbValue, $this->mockPlatform);
+        $this->assertInstanceOf(GeoPosition::class, $resultingPhpValue);
+        $this->assertTrue($resultingPhpValue->isEqual($phpValue));
+    }
+
+    public function testItConvertsToPhpNullValue(): void
+    {
+        $resultingPhpValue = $this->type->convertToPHPValue(null, $this->mockPlatform);
+        $this->assertNull($resultingPhpValue);
+    }
+
+    public function testItFailsToConvertDatabaseInvalidJsonValueToPhpValue(): void
+    {
+        $valueOfInvalidType = 'value of invalid type';
+        $this->expectException(ConversionException::class);
+        $this->expectExceptionMessage(\sprintf(
+            'Could not convert database value "%s" to Doctrine Type %s',
+            $valueOfInvalidType,
+            $this->typeName,
+        ));
+        $this->type->convertToPHPValue($valueOfInvalidType, $this->mockPlatform);
+    }
+
+    public function testItFailsToConvertDatabaseIncompleteValueToPhpValue(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Неверный формат геопозиции: "{}".');
+        $this->type->convertToPHPValue('{}', $this->mockPlatform);
+    }
+
+    private function getConversionTests(): array
+    {
+        return [
+            // database value,
+            // PHP value
+            [
+                '{"coordinates":{"latitude":54.950357,"longitude":-172.7972252},"accuracy":0.25}',
+                new GeoPosition(new Coordinates('54.950357', '-172.7972252'), new Accuracy('0.25'))
+            ],
+            [
+                '{"coordinates":{"latitude":-10.950357,"longitude":72.7972252},"accuracy":null}',
+                new GeoPosition(new Coordinates('-10.950357','72.7972252'), null)
+            ],
+        ];
+    }
+}
