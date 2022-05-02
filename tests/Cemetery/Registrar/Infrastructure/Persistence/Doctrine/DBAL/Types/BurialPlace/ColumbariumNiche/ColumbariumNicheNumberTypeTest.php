8f0b51abe5ca764a3d7fbbcfb897c47de438e178
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-03 01:00:36 +0700

    add Doctrine DBAL custom type for columbarium niche number

---
+++ tests/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/DBAL/Types/BurialPlace/ColumbariumNiche/ColumbariumNicheNumberTypeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\BurialPlace\ColumbariumNiche;
+
+use Cemetery\Registrar\Domain\BurialPlace\ColumbariumNiche\ColumbariumNicheNumber;
+use Cemetery\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\BurialPlace\ColumbariumNiche\ColumbariumNicheNumberType;
+use Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\CustomStringTypeTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class ColumbariumNicheNumberTypeTest extends CustomStringTypeTest
+{
+    protected string $className = ColumbariumNicheNumberType::class;
+    protected string $typeName  = 'columbarium_niche_number';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dbValue  = '001';
+        $this->phpValue = new ColumbariumNicheNumber('001');
+    }
+}
