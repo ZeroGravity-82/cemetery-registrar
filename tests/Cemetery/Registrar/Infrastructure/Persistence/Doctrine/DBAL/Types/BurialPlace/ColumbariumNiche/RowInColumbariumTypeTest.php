6576f8be5207475b61445028bec536315fc3ad50
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-03 00:58:19 +0700

    add Doctrine DBAL custom type for row in columbarium

---
+++ tests/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/DBAL/Types/BurialPlace/ColumbariumNiche/RowInColumbariumTypeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\BurialPlace\ColumbariumNiche;
+
+use Cemetery\Registrar\Domain\BurialPlace\ColumbariumNiche\RowInColumbarium;
+use Cemetery\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\BurialPlace\ColumbariumNiche\RowInColumbariumType;
+use Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\DBAL\Types\CustomIntegerTypeTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class RowInColumbariumTypeTest extends CustomIntegerTypeTest
+{
+    protected string $className = RowInColumbariumType::class;
+    protected string $typeName  = 'row_in_columbarium';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dbValue  = 10;
+        $this->phpValue = new RowInColumbarium(10);
+    }
+}
