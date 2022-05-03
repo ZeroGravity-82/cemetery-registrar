f3d10649060b4fff73f62865a66454490a15025d
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-03 13:09:11 +0700

    fix tests for ColumbariumRepository class

--- tests/Cemetery/Registrar/Infrastructure/Domain/BurialPlace/ColumbariumNiche/Doctrine/ORM/ColumbariumRepositoryIntegrationTest.php
+++ tests/Cemetery/Registrar/Infrastructure/Domain/BurialPlace/ColumbariumNiche/Doctrine/ORM/ColumbariumRepositoryIntegrationTest.php
@@ -177,26 +177,6 @@ class ColumbariumRepositoryIntegrationTest extends AbstractRepositoryIntegration
         $this->assertNotNull($this->getRemovedAtTimestampById(Columbarium::class, (string) $this->columbariumA->id()));
     }
 
-    public function testItHydratesGeoPositionEmbeddable(): void
-    {
-        $this->repo->saveAll(new ColumbariumCollection([$this->columbariumA, $this->columbariumB, $this->columbariumC]));
-        $this->entityManager->clear();
-
-        $persistedColumbarium = $this->repo->findById($this->columbariumA->id());
-        $this->assertInstanceOf(Columbarium::class, $persistedColumbarium);
-        $this->assertNull($persistedColumbarium->geoPosition());
-
-        $persistedColumbarium = $this->repo->findById($this->columbariumB->id());
-        $this->assertInstanceOf(Columbarium::class, $persistedColumbarium);
-        $this->assertInstanceOf(GeoPosition::class, $persistedColumbarium->geoPosition());
-        $this->assertTrue($persistedColumbarium->geoPosition()->isEqual($this->columbariumB->geoPosition()));
-
-        $persistedColumbarium = $this->repo->findById($this->columbariumC->id());
-        $this->assertInstanceOf(Columbarium::class, $persistedColumbarium);
-        $this->assertInstanceOf(GeoPosition::class, $persistedColumbarium->geoPosition());
-        $this->assertTrue($persistedColumbarium->geoPosition()->isEqual($this->columbariumC->geoPosition()));
-    }
-
     public function testItRemovesACollectionOfColumbariums(): void
     {
         // Prepare the repo for testing
