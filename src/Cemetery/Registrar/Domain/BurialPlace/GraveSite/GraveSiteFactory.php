0e677fdc4b868605963db941f13475a13ff8fe6f
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-10 19:09:54 +0700

    add factory for grave site entity

---
+++ src/Cemetery/Registrar/Domain/BurialPlace/GraveSite/GraveSiteFactory.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\EntityFactory;
+use Cemetery\Registrar\Domain\GeoPosition\Coordinates;
+use Cemetery\Registrar\Domain\GeoPosition\Error;
+use Cemetery\Registrar\Domain\GeoPosition\GeoPosition;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class GraveSiteFactory extends EntityFactory
+{
+    /**
+     * @param string|null $cemeteryBlockId
+     * @param int|null    $rowInBlock
+     * @param int|null    $positionInRow
+     * @param string|null $geoPositionLatitude
+     * @param string|null $geoPositionLongitude
+     * @param string|null $geoPositionError
+     * @param string|null $size
+     *
+     * @return GraveSite
+     */
+    public function create(
+        ?string $cemeteryBlockId,
+        ?int    $rowInBlock,
+        ?int    $positionInRow,
+        ?string $geoPositionLatitude,
+        ?string $geoPositionLongitude,
+        ?string $geoPositionError,
+        ?string $size,
+    ): GraveSite {
+        $cemeteryBlockId        = new CemeteryBlockId((string) $cemeteryBlockId);
+        $rowInBlock             = new RowInBlock((int) $rowInBlock);
+        $positionInRow          = $positionInRow !== null ? new PositionInRow($positionInRow) : null;
+        $geoPositionCoordinates = $geoPositionLatitude !== null && $geoPositionLongitude !== null
+            ? new Coordinates($geoPositionLatitude, $geoPositionLongitude)
+            : null;
+        $geoPositionError = $geoPositionError !== null
+            ? new Error($geoPositionError)
+            : null;
+        $geoPosition = $geoPositionCoordinates !== null
+            ? new GeoPosition($geoPositionCoordinates, $geoPositionError)
+            : null;
+        $size = $size !== null ? new GraveSiteSize($size) : null;
+
+        return (new GraveSite(
+                new GraveSiteId($this->identityGenerator->getNextIdentity()),
+                $cemeteryBlockId,
+                $rowInBlock,
+            ))
+            ->setPositionInRow($positionInRow)
+            ->setGeoPosition($geoPosition)
+            ->setSize($size);
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/BurialPlace/GraveSite/GraveSiteFactoryTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\BurialPlace\GraveSite\GraveSite;
+use Cemetery\Registrar\Domain\BurialPlace\GraveSite\GraveSiteFactory;
+use Cemetery\Tests\Registrar\Domain\EntityFactoryTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class GraveSiteFactoryTest extends EntityFactoryTest
+{
+    private GraveSiteFactory $graveSiteFactory;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->graveSiteFactory = new GraveSiteFactory($this->mockIdentityGenerator);
+    }
+
+    public function testItCreatesColumbariumNiche(): void
+    {
+        $cemeteryBlockId      = 'CB01';
+        $rowInBlock           = 5;
+        $positionInRow        = 10;
+        $geoPositionLatitude  = '54.950357';
+        $geoPositionLongitude = '82.7972252';
+        $geoPositionError     = '0.2';
+        $size                 = '3.0';
+        $this->mockIdentityGenerator->expects($this->once())->method('getNextIdentity');
+        $graveSite = $this->graveSiteFactory->create(
+            $cemeteryBlockId,
+            $rowInBlock,
+            $positionInRow,
+            $geoPositionLatitude,
+            $geoPositionLongitude,
+            $geoPositionError,
+            $size,
+        );
+        $this->assertInstanceOf(GraveSite::class, $graveSite);
+        $this->assertSame(self::ENTITY_ID, $graveSite->id()->value());
+        $this->assertSame($cemeteryBlockId, $graveSite->cemeteryBlockId()->value());
+        $this->assertSame($rowInBlock, $graveSite->rowInBlock()->value());
+        $this->assertSame($positionInRow, $graveSite->positionInRow()->value());
+        $this->assertSame($geoPositionLatitude, $graveSite->geoPosition()->coordinates()->latitude());
+        $this->assertSame($geoPositionLongitude, $graveSite->geoPosition()->coordinates()->longitude());
+        $this->assertSame($geoPositionError, $graveSite->geoPosition()->error()->value());
+        $this->assertSame($size, $graveSite->size()->value());
+    }
+
+    public function testItCreatesColumbariumNicheWithoutOptionalFields(): void
+    {
+        $cemeteryBlockId = 'CB01';
+        $rowInBlock      = 5;
+        $this->mockIdentityGenerator->expects($this->once())->method('getNextIdentity');
+        $graveSite = $this->graveSiteFactory->create(
+            $cemeteryBlockId,
+            $rowInBlock,
+            null,
+            null,
+            null,
+            null,
+            null,
+        );
+        $this->assertInstanceOf(GraveSite::class, $graveSite);
+        $this->assertSame(self::ENTITY_ID, $graveSite->id()->value());
+        $this->assertSame($cemeteryBlockId, $graveSite->cemeteryBlockId()->value());
+        $this->assertSame($rowInBlock, $graveSite->rowInBlock()->value());
+        $this->assertNull($graveSite->positionInRow());
+        $this->assertNull($graveSite->geoPosition());
+        $this->assertNull($graveSite->size());
+    }
+}
