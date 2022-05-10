3eb8b2ad84b50d24cf9d05f715e285b2b759777f
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-10 20:49:00 +0700

    reformat NaturalPerson class

--- src/Cemetery/Registrar/Domain/NaturalPerson/NaturalPerson.php
+++ src/Cemetery/Registrar/Domain/NaturalPerson/NaturalPerson.php
@@ -14,8 +14,8 @@ use Cemetery\Registrar\Domain\Contact\PhoneNumber;
  */
 final class NaturalPerson extends AggregateRoot
 {
-    public const CLASS_SHORTCUT  = 'NATURAL_PERSON';
-    public const CLASS_LABEL     = 'физическое лицо';
+    public const CLASS_SHORTCUT = 'NATURAL_PERSON';
+    public const CLASS_LABEL    = 'физическое лицо';
 
     /**
      * @var PhoneNumber|null
