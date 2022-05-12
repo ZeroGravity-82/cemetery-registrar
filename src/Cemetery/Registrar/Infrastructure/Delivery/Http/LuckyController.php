68736d16fdacf8522e1da4daa251de2415467060
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-12 15:14:35 +0700

    add test controller and twig template

---
+++ src/Cemetery/Registrar/Infrastructure/Delivery/Http/LuckyController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Infrastructure\Delivery\Http;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/lucky/number', name: 'lucky_num')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
