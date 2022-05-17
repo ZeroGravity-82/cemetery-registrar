977feee0123ef5fabc0b567fecc285e2d03120c2
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-17 14:31:58 +0700

    add Doctrine DBAL custom type for place of birth

---
+++ src/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/Dbal/Types/NaturalPerson/PlaceOfBirthType.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\NaturalPerson;
+
+use Cemetery\Registrar\Domain\NaturalPerson\PlaceOfBirth;
+use Cemetery\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\CustomStringType;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class PlaceOfBirthType extends CustomStringType
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected string $className = PlaceOfBirth::class;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected string $typeName  = 'place_of_birth';
+}
---
+++ tests/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/Dbal/Types/NaturalPerson/PlaceOfBirthTypeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\NaturalPerson;
+
+use Cemetery\Registrar\Domain\NaturalPerson\PlaceOfBirth;
+use Cemetery\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\NaturalPerson\PlaceOfBirthType;
+use Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\CustomStringTypeTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class PlaceOfBirthTypeTest extends CustomStringTypeTest
+{
+    protected string $className = PlaceOfBirthType::class;
+    protected string $typeName  = 'place_of_birth';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dbValue  = 'город Новосибирск';
+        $this->phpValue = new PlaceOfBirth('город Новосибирск');
+    }
+}
