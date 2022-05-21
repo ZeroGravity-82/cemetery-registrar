aefea2dc2e848f4e29af54473af94d6ebc8ac7b3
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-21 21:15:46 +0700

    add value object for burial chain ID

---
+++ src/Cemetery/Registrar/Domain/Burial/BurialChainId.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Burial;
+
+use Cemetery\Registrar\Domain\EntityId;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class BurialChainId extends EntityId
+{
+}
