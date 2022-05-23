f0826d4c08fd7af4b6e200ea209c36908e207075
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-23 21:25:27 +0700

    fix burial index page rendering

--- src/Cemetery/Registrar/Infrastructure/Delivery/Http/BurialController.php
+++ src/Cemetery/Registrar/Infrastructure/Delivery/Http/BurialController.php
@@ -4,16 +4,27 @@ declare(strict_types=1);
 
 namespace Cemetery\Registrar\Infrastructure\Delivery\Http;
 
+use Cemetery\Registrar\Application\Burial\BurialFetcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BurialController extends AbstractController
 {
+    /**
+     * @param BurialFetcher $burialFetcher
+     */
+    public function __construct(
+        private readonly BurialFetcher $burialFetcher,
+    ) {}
+
     #[Route('/burial', name: 'burial_index', methods: 'GET')]
     public function index(): Response
     {
+        $burialViewList = $this->burialFetcher->findAll(1);
+
         return $this->render('burial/index.html.twig', [
+            'burialViewList' => $burialViewList,
         ]);
     }
 }
