1daa6a578abb363a124c1964270203efe37f834f
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-30 13:56:17 +0700

    add ucfirst extension for Twig

---
+++ src/Cemetery/Registrar/Infrastructure/Delivery/Http/Twig/AppExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Infrastructure\Delivery\Http\Twig;
+
+use Twig\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class AppExtension extends AbstractExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('ucfirst', [$this, 'ucfirst'], ['needs_environment' => true]),
+        ];
+    }
+
+    /**
+     * @param Environment $environment
+     * @param string      $value
+     *
+     * @return string
+     */
+    public function ucfirst(Environment $environment, string $value): string
+    {
+        $charset = $environment->getCharset();
+        if ($charset) {
+            $result =
+                \mb_strtoupper(\mb_substr($value, 0, 1, $charset), $charset) .
+                \mb_substr($value, 1, \mb_strlen($value, $charset));
+        } else {
+            $result = \ucfirst($value);
+        }
+
+        return $result;
+    }
+}
