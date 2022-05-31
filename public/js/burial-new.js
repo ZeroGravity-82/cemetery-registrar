be9acac7f7bc216c091654013339cee193986094
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-05-31 12:35:50 +0700

    add switching for customer type selector on new burial modal

--- public/js/burial-new.js
+++ public/js/burial-new.js
@@ -1,7 +1,35 @@
-const $customerTypeSelector = $('.js-customer-type-selector');
-const naturalPersonValue = $customerTypeSelector.data('natural-person-value');
+const $customerTypeSelectorWrapper = $(`.js-customer-type-selector-wrapper`);
+const naturalPersonTypeValue       = $customerTypeSelectorWrapper.data(`natural-person-value`);
+const juristicPersonTypeValue      = $customerTypeSelectorWrapper.data(`juristic-person-value`);
+const soleProprietorTypeValue      = $customerTypeSelectorWrapper.data(`sole-proprietor-value`);
+const $customerTypeSelector        = $customerTypeSelectorWrapper.find(`input:radio[name=customerType]`);
+const $naturalPersonSubform        = $(`.js-natural-person-subform`);
+const $juristicPersonSubform       = $(`.js-juristic-person-subform`);
+const $soleProprietorSubform       = $(`.js-sole-proprietor-subform`);
 
-const $customerTypeSelector = $('.js-customer-type-selector input:radio[name=customerType]');
-if($customerTypeSelector.is(':checked') === false) {
-    $customerTypeSelector.filter('[value=Male]').prop('checked', true);
+// Initialize customer type selector
+if($customerTypeSelector.is(`:checked`) === false) {
+    $customerTypeSelector.filter(`[value=${naturalPersonTypeValue}]`).prop(`checked`, true);
 }
+$naturalPersonSubform.removeClass(`d-none`);
+
+// Switch customer type
+$customerTypeSelector.on(`change`, function (event) {
+    $naturalPersonSubform.removeClass(`d-none`).addClass(`d-none`);
+    $juristicPersonSubform.removeClass(`d-none`).addClass(`d-none`);
+    $soleProprietorSubform.removeClass(`d-none`).addClass(`d-none`);
+
+    const $target                   = $(event.target);
+    const customerTypeSelectorValue = $target.val();
+    switch (customerTypeSelectorValue) {
+        case naturalPersonTypeValue:
+            $naturalPersonSubform.removeClass(`d-none`);
+            break;
+        case juristicPersonTypeValue:
+            $juristicPersonSubform.removeClass(`d-none`);
+            break;
+        case soleProprietorTypeValue:
+            $soleProprietorSubform.removeClass(`d-none`);
+            break;
+    }
+});
