08fb8fa7189df74ba92d5c414723df23fd2ab60e
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-06-05 12:17:17 +0700

    [wip] add funeral company page

--- src/Cemetery/Registrar/Application/FuneralCompany/FuneralCompanyViewListItem.php
+++ src/Cemetery/Registrar/Application/FuneralCompany/FuneralCompanyViewListItem.php
@@ -12,5 +12,12 @@ final class FuneralCompanyViewListItem
     public function __construct(
         public readonly string $id,
         public readonly string $name,
+        public readonly ?string $inn,
+        public readonly ?string $juristicPersonLegalAddress,
+        public readonly ?string $juristicPersonPostalAddress,
+        public readonly ?string $juristicPersonPhone,
+        public readonly ?string $soleProprietorRegistrationAddress,
+        public readonly ?string $soleProprietorActualLocationAddress,
+        public readonly ?string $soleProprietorPhone,
     ) {}
 }
---
+++ src/Cemetery/Registrar/Infrastructure/Delivery/Http/FuneralCompanyController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Infrastructure\Delivery\Http;
+
+use Cemetery\Registrar\Application\FuneralCompany\FuneralCompanyFetcher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class FuneralCompanyController extends AbstractController
+{
+    /**
+     * @param FuneralCompanyFetcher $funeralCompanyFetcher
+     */
+    public function __construct(
+//        private readonly FuneralCompanyFetcher $funeralCompanyFetcher,
+    ) {}
+
+    #[Route('/funeral-company', name: 'funeral_company_index', methods: 'GET')]
+    public function index(): Response
+    {
+        $funeralCompanyViewList   = $this->funeralCompanyFetcher->findAll(1);
+        $funeralCompanyTotalCount = $this->funeralCompanyFetcher->getTotalCount();
+
+        return $this->render('funeral_company/index.html.twig', [
+            'funeralCompanyViewList'   => $funeralCompanyViewList,
+            'funeralCompanyTotalCount' => $funeralCompanyTotalCount,
+        ]);
+    }
+}
