ec85e5236f9563f6ff6d4306e1b7c7fed73d1eac
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-06-07 14:14:27 +0700

    add dependency on dama/doctrine-test-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
