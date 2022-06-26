97cd6dd21094243a107c141902ea2012d0445c5c
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-06-26 12:38:40 +0700

    reformat tests for coffin shape value object

--- tests/Cemetery/Registrar/Domain/Model/Burial/BurialContainer/CoffinShapeTest.php
+++ tests/Cemetery/Registrar/Domain/Model/Burial/BurialContainer/CoffinShapeTest.php
@@ -21,7 +21,7 @@ class CoffinShapeTest extends TestCase
     private const AMERICAN_VALUE              = 'AMERICAN';
     private const AMERICAN_LABEL              = 'американец';
 
-    public function testItHasValidShapeValueConstants(): void
+    public function testItHasValidValueConstants(): void
     {
         $this->assertSame(self::TRAPEZOID_VALUE,             CoffinShape::TRAPEZOID);
         $this->assertSame(self::GREEK_WITH_HANDLES_VALUE,    CoffinShape::GREEK_WITH_HANDLES);
@@ -29,7 +29,7 @@ class CoffinShapeTest extends TestCase
         $this->assertSame(self::AMERICAN_VALUE,              CoffinShape::AMERICAN);
     }
 
-    public function testItHasValidShapeLabesConstants(): void
+    public function testItHasValidLabelConstants(): void
     {
         $this->assertSame(self::TRAPEZOID_LABEL,             CoffinShape::LABELS[self::TRAPEZOID_VALUE]);
         $this->assertSame(self::GREEK_WITH_HANDLES_LABEL,    CoffinShape::LABELS[self::GREEK_WITH_HANDLES_VALUE]);
