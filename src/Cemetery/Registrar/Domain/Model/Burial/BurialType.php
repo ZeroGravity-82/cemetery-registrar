86eb2a06d33c2bc706a48908c2576ceec76404c9
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-06-26 12:42:53 +0700

    fix labels constant name for burial type value object

--- src/Cemetery/Registrar/Domain/Model/Burial/BurialType.php
+++ src/Cemetery/Registrar/Domain/Model/Burial/BurialType.php
@@ -14,7 +14,7 @@ class BurialType
     public const URN_IN_COLUMBARIUM_NICHE  = 'URN_IN_COLUMBARIUM_NICHE';
     public const ASHES_UNDER_MEMORIAL_TREE = 'ASHES_UNDER_MEMORIAL_TREE';
 
-    public const TYPE_LABELS = [
+    public const LABELS = [
         self::COFFIN_IN_GRAVE_SITE      => 'гробом в могилу',
         self::URN_IN_GRAVE_SITE         => 'урной в могилу',
         self::URN_IN_COLUMBARIUM_NICHE  => 'урной в колумбарную нишу',
@@ -83,7 +83,7 @@ class BurialType
      */
     public function label(): string
     {
-        return self::TYPE_LABELS[$this->value()];
+        return self::LABELS[$this->value()];
     }
 
 
