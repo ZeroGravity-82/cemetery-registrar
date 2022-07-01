db415d52e0c619ccb482928745ebcc8a1eb21092
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-07-01 16:45:59 +0700

    add deceased details to natural person mapping

---
+++ migrations/Version20220701094332.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220701094332 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE natural_person ADD deceased_details JSON DEFAULT NULL COMMENT \'(DC2Type:deceased_details)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE natural_person DROP deceased_details');
+    }
+}
