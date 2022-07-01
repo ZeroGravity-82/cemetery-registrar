b4185eb3aab04d5c4daa478aa32daa53f7490369
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-07-01 16:39:45 +0700

    add custom Doctrine DBAL type for deceased details

---
+++ src/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/Dbal/Types/NaturalPerson/DeceasedDetails/DeceasedDetailsType.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\NaturalPerson\DeceasedDetails;
+
+use Cemetery\Registrar\Domain\Model\CauseOfDeath\CauseOfDeathId;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\DeceasedDetails\Age;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\DeceasedDetails\CremationCertificate;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\DeceasedDetails\DeathCertificate;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\DeceasedDetails\DeceasedDetails;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\NaturalPersonId;
+use Cemetery\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\CustomJsonType;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class DeceasedDetailsType extends CustomJsonType
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected string $className = DeceasedDetails::class;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected string $typeName = 'deceased_details';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function assertValidDecodedValue(mixed $decodedValue, mixed $value): void
+    {
+        $isInvalidValue =
+            !\array_key_exists('naturalPersonId',      $decodedValue) ||
+            !\array_key_exists('diedAt',               $decodedValue) ||
+            !\array_key_exists('age',                  $decodedValue) ||
+            !\array_key_exists('causeOfDeathId',       $decodedValue) ||
+            !\array_key_exists('deathCertificate',     $decodedValue) ||
+            !\array_key_exists('cremationCertificate', $decodedValue);
+        if (!$isInvalidValue) {
+            $isInvalidValue = \is_array($decodedValue['deathCertificate']) && (
+                !\array_key_exists('series', $decodedValue['deathCertificate']) ||
+                !\array_key_exists('number', $decodedValue['deathCertificate']) ||
+                !\array_key_exists('issuedAt', $decodedValue['deathCertificate'])
+            );
+        }
+        if (!$isInvalidValue) {
+            $isInvalidValue = \is_array($decodedValue['cremationCertificate']) && (
+                !\array_key_exists('number', $decodedValue['cremationCertificate']) ||
+                !\array_key_exists('issuedAt', $decodedValue['cremationCertificate'])
+            );
+        }
+
+        if ($isInvalidValue) {
+            throw new \RuntimeException(\sprintf('Неверный формат данных умершего: "%s".', $value));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function preparePhpValueForJsonEncoding(mixed $value): array
+    {
+        /** @var DeceasedDetails $value */
+        return [
+            'naturalPersonId'  => $value->naturalPersonId()->value(),
+            'diedAt'           => $value->diedAt()->format('Y-m-d'),
+            'age'              => $value->age() ? (float) $value->age()->value() : null,
+            'causeOfDeathId'   => $value->causeOfDeathId()?->value(),
+            'deathCertificate' => $value->deathCertificate()
+                ? [
+                    'series'   => $value->deathCertificate()->series(),
+                    'number'   => $value->deathCertificate()->number(),
+                    'issuedAt' => $value->deathCertificate()->issuedAt()->format('Y-m-d'),
+                ]
+                : null,
+            'cremationCertificate' => $value->cremationCertificate()
+                ? [
+                    'number'   => $value->cremationCertificate()->number(),
+                    'issuedAt' => $value->cremationCertificate()->issuedAt()->format('Y-m-d'),
+                ]
+                : null,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function buildPhpValue(array $decodedValue): DeceasedDetails
+    {
+        return new DeceasedDetails(
+            new NaturalPersonId($decodedValue['naturalPersonId']),
+            \DateTimeImmutable::createFromFormat('Y-m-d', $decodedValue['diedAt']),
+            !\is_null($decodedValue['age'])            ? new Age($decodedValue['age'])                       : null,
+            !\is_null($decodedValue['causeOfDeathId']) ? new CauseOfDeathId($decodedValue['causeOfDeathId']) : null,
+            !\is_null($decodedValue['deathCertificate'])
+                ? new DeathCertificate(
+                    $decodedValue['deathCertificate']['series'],
+                    $decodedValue['deathCertificate']['number'],
+                    \DateTimeImmutable::createFromFormat('Y-m-d', $decodedValue['deathCertificate']['issuedAt']),
+                )
+                : null,
+            !\is_null($decodedValue['cremationCertificate'])
+                ? new CremationCertificate(
+                    $decodedValue['cremationCertificate']['number'],
+                    \DateTimeImmutable::createFromFormat('Y-m-d', $decodedValue['cremationCertificate']['issuedAt']),
+                )
+                : null,
+        );
+    }
+}
---
+++ tests/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/Dbal/Types/NaturalPerson/DeceasedDetails/DeceasedDetailsTypeTest.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\NaturalPerson\DeceasedDetails;
+
+use Cemetery\Registrar\Domain\Model\CauseOfDeath\CauseOfDeathId;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\DeceasedDetails\Age;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\DeceasedDetails\CremationCertificate;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\DeceasedDetails\DeathCertificate;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\DeceasedDetails\DeceasedDetails;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\NaturalPersonId;
+use Cemetery\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\NaturalPerson\DeceasedDetails\DeceasedDetailsType;
+use Cemetery\Tests\Registrar\Infrastructure\Persistence\Doctrine\Dbal\Types\CustomJsonTypeTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class DeceasedDetailsTypeTest extends CustomJsonTypeTest
+{
+    protected string $className                                  = DeceasedDetailsType::class;
+    protected string $typeName                                   = 'deceased_details';
+    protected string $phpValueClassName                          = DeceasedDetails::class;
+    protected string $exceptionMessageForDatabaseIncompleteValue = 'Неверный формат данных умершего';
+
+    /**
+     * @dataProvider getConversionTests
+     */
+    public function testItConvertsToDatabaseValue(string $dbValue, DeceasedDetails $phpValue): void
+    {
+        $resultingDbValue = $this->type->convertToDatabaseValue($phpValue, $this->mockPlatform);
+        $this->assertJson($resultingDbValue);
+        $decodedResultDbValue = \json_decode($resultingDbValue, true);
+        $this->assertIsArray($decodedResultDbValue);
+        $this->assertArrayHasKey('naturalPersonId', $decodedResultDbValue);
+        $this->assertArrayHasKey('diedAt', $decodedResultDbValue);
+        $this->assertArrayHasKey('age', $decodedResultDbValue);
+        $this->assertArrayHasKey('causeOfDeathId', $decodedResultDbValue);
+        $this->assertArrayHasKey('deathCertificate', $decodedResultDbValue);
+        $this->assertArrayHasKey('cremationCertificate', $decodedResultDbValue);
+        $this->assertSame($phpValue->naturalPersonId()->value(), $decodedResultDbValue['naturalPersonId']);
+        $this->assertSame($phpValue->diedAt()->format('Y-m-d'), $decodedResultDbValue['diedAt']);
+        $this->assertSame($phpValue->age()?->value(), $decodedResultDbValue['age']);
+        $this->assertSame($phpValue->causeOfDeathId()?->value(), $decodedResultDbValue['causeOfDeathId']);
+        $this->assertSame(
+            $phpValue->deathCertificate()?->series(),
+            $decodedResultDbValue['deathCertificate']['series'] ?? null
+        );
+        $this->assertSame(
+            $phpValue->deathCertificate()?->number(),
+            $decodedResultDbValue['deathCertificate']['number'] ?? null
+        );
+        $this->assertSame(
+            $phpValue->deathCertificate()?->issuedAt()->format('Y-m-d'),
+            $decodedResultDbValue['deathCertificate']['issuedAt'] ?? null
+        );
+        $this->assertSame(
+            $phpValue->cremationCertificate()?->number(),
+            $decodedResultDbValue['cremationCertificate']['number'] ?? null
+        );
+        $this->assertSame(
+            $phpValue->cremationCertificate()?->issuedAt()->format('Y-m-d'),
+            $decodedResultDbValue['cremationCertificate']['issuedAt'] ?? null
+        );
+    }
+
+    /**
+     * @dataProvider getConversionTests
+     */
+    public function testItConvertsToPhpValue(string $dbValue, DeceasedDetails $phpValue): void
+    {
+        $resultingPhpValue = $this->type->convertToPHPValue($dbValue, $this->mockPlatform);
+        $this->assertInstanceOf(DeceasedDetails::class, $resultingPhpValue);
+        $this->assertTrue($resultingPhpValue->isEqual($phpValue));
+    }
+
+    protected function getConversionTests(): array
+    {
+        return [
+            // database value,
+            // PHP value
+            [
+                <<<JSON_A
+{
+  "naturalPersonId": "NP001",
+  "diedAt": "2011-04-30",
+  "age": 82,
+  "causeOfDeathId": "CD001",
+  "deathCertificate": {
+    "series": "V-МЮ",
+    "number": "532515",
+    "issuedAt": "2002-10-28"
+  },
+  "cremationCertificate": {
+    "number": "12964",
+    "issuedAt": "2002-10-29"
+  }
+}
+JSON_A
+        ,
+                new DeceasedDetails(
+                    new NaturalPersonId('NP001'),
+                    new \DateTimeImmutable('2011-04-30'),
+                    new Age(82),
+                    new CauseOfDeathId('CD001'),
+                    new DeathCertificate('V-МЮ', '532515', new \DateTimeImmutable('2002-10-28')),
+                    new CremationCertificate('12964', new \DateTimeImmutable('2002-10-29')),
+                ),
+            ],
+            [
+                <<<JSON_B
+{
+  "naturalPersonId": "NP002",
+  "diedAt": "2021-12-15",
+  "age": null,
+  "causeOfDeathId": null,
+  "deathCertificate": null,
+  "cremationCertificate": null
+}
+JSON_B
+                ,
+                new DeceasedDetails(
+                    new NaturalPersonId('NP002'),
+                    new \DateTimeImmutable('2021-12-15'),
+                    null,
+                    null,
+                    null,
+                    null,
+                )
+            ],
+        ];
+    }
+}
