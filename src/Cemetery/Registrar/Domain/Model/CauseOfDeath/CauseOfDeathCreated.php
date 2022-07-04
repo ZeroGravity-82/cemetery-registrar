82b003eff4afe1a228228551ecf6a3f7ff1cd879
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-07-04 11:54:19 +0700

    add event about cause of death creation

---
+++ src/Cemetery/Registrar/Domain/Model/CauseOfDeath/CauseOfDeathCreated.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Model\CauseOfDeath;
+
+use Cemetery\Registrar\Domain\Model\Event;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class CauseOfDeathCreated extends Event
+{
+    /**
+     * @param CauseOfDeathId   $causeOfDeathId
+     * @param CauseOfDeathName $causeOfDeathName
+     */
+    public function __construct(
+        private readonly CauseOfDeathId   $causeOfDeathId,
+        private readonly CauseOfDeathName $causeOfDeathName,
+    ) {
+        parent::__construct();
+    }
+
+    /**
+     * @return CauseOfDeathId
+     */
+    public function causeOfDeathId(): CauseOfDeathId
+    {
+        return $this->causeOfDeathId;
+    }
+
+    /**
+     * @return CauseOfDeathName
+     */
+    public function causeOfDeathName(): CauseOfDeathName
+    {
+        return $this->causeOfDeathName;
+    }
+}
