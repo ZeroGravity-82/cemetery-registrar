4418ad714f537d7103dafb513d734e58569aaefa
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-07-06 16:35:32 +0700

    [wip]

---
+++ public/js/cause-of-death.js
@@ -0,0 +1,31 @@
+const $body              = $(`body`);
+const $modalCauseOfDeath = $(`#modalCauseOfDeath`);
+const $modalTitle        = $modalCauseOfDeath.find(`.modal-title`)
+const $modalNameField    = $modalCauseOfDeath.find(`input[id=name]`);
+const $modalTimestamps   = $modalCauseOfDeath.find(`.timestamps`);
+
+const modalCauseOfDeath  = new bootstrap.Modal(`#modalCauseOfDeath`, {});
+
+$body.on(`click`, `.js-create-cause-of-death-btn`, function() {
+    $modalCauseOfDeath.removeClass(`edit-form`);
+    $modalTimestamps.removeClass(`d-none`).addClass(`d-none`);
+    $modalTitle.html(`Причины смерти (создание)`);
+    $modalNameField.val(null);
+    modalCauseOfDeath.show();
+});
+
+$body.on(`click`, `tr`, function(e) {
+    const $tr = $(e.target).closest(`tr`);
+    const id  = $tr.attr(`data-id`);
+    $.ajax({
+        type: `GET`,
+        url: `/admin/cause-of-death/edit/${id}`,
+        success: function (causeOfDeathView) {
+            $modalCauseOfDeath.removeClass(`edit-form`).addClass(`edit-form`);
+            $modalTimestamps.removeClass(`d-none`);
+            $modalTitle.html(`${causeOfDeathView.name} (Причины смерти)`);
+            $modalNameField.val(causeOfDeathView.name);
+            modalCauseOfDeath.show();
+        }
+    });
+});
--- src/Cemetery/Registrar/Infrastructure/Delivery/Web/Controller/Admin/AdminCauseOfDeathController.php
+++ src/Cemetery/Registrar/Infrastructure/Delivery/Web/Controller/Admin/AdminCauseOfDeathController.php
@@ -10,7 +10,10 @@ use Cemetery\Registrar\Application\Query\CauseOfDeath\CountCauseOfDeathTotal\Cou
 use Cemetery\Registrar\Application\Query\CauseOfDeath\CountCauseOfDeathTotal\CountCauseOfDeathTotalService;
 use Cemetery\Registrar\Application\Query\CauseOfDeath\ListCausesOfDeath\ListCausesOfDeathRequest;
 use Cemetery\Registrar\Application\Query\CauseOfDeath\ListCausesOfDeath\ListCausesOfDeathService;
+use Cemetery\Registrar\Domain\Model\CauseOfDeath\CauseOfDeathRepository;
+use Cemetery\Registrar\Domain\View\CauseOfDeath\CauseOfDeathFetcher;
 use Cemetery\Registrar\Infrastructure\Delivery\Web\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,17 +26,22 @@ class AdminCauseOfDeathController extends Controller
     /**
      * @param CountCauseOfDeathTotalService $countCauseOfDeathTotalService
      * @param ListCausesOfDeathService      $listCausesOfDeathService
+     * @param CreateCauseOfDeathService     $createCauseOfDeathService
+     * @param CauseOfDeathRepository        $causeOfDeathRepo
+     * @param CauseOfDeathFetcher           $causeOfDeathFetcher
      */
     public function __construct(
         private readonly CountCauseOfDeathTotalService $countCauseOfDeathTotalService,
         private readonly ListCausesOfDeathService      $listCausesOfDeathService,
         private readonly CreateCauseOfDeathService     $createCauseOfDeathService,
+        private readonly CauseOfDeathRepository        $causeOfDeathRepo,
+        private readonly CauseOfDeathFetcher           $causeOfDeathFetcher,
     ) {}
 
     /**
      * @return Response
      */
-    #[Route('/admin/cause-of-death', name: 'admin_cause_of_death_list', methods: 'GET')]
+    #[Route('/admin/cause-of-death', name: 'admin_cause_of_death_list', methods: Request::METHOD_GET)]
     public function list(): Response
     {
         $causeOfDeathTotalCount = $this->countCauseOfDeathTotalService
@@ -54,15 +62,38 @@ class AdminCauseOfDeathController extends Controller
      *
      * @return Response
      */
-    #[Route('/admin/cause-of-death/new', name: 'admin_cause_of_death_new', methods: 'POST')]
+    #[Route('/admin/cause-of-death/new', name: 'admin_cause_of_death_new', methods: Request::METHOD_POST)]
     public function new(Request $request): Response
     {
-        $createCauseOfDeathRequest = new CreateCauseOfDeathRequest($this->getInputString($request, 'name'));
+        $name                      = $this->getInputString($request, 'name');
+        dump($name);
+
+
+        $createCauseOfDeathRequest = new CreateCauseOfDeathRequest($name);
         $causeOfDeathId            = $this->createCauseOfDeathService
             ->execute($createCauseOfDeathRequest)->causeOfDeathId;
-        $response = $this->redirectToRoute('admin_cause_of_death_list', [
+        $response                  = $this->redirectToRoute('admin_cause_of_death_list', [
                 'causeOfDeathId' => $causeOfDeathId,
             ]);
+
+        return $response;
+    }
+
+    #[Route('/admin/cause-of-death/edit/{id}', name: 'admin_cause_of_death_edit', methods: [
+        Request::METHOD_GET,
+        Request::METHOD_POST,
+    ])]
+    public function edit(Request $request, string $id): JsonResponse
+    {
+        if ($request->isMethod(Request::METHOD_POST)) {
+            $burial = $this->causeOfDeathRepo->findById($id);
+        }
+
+        if ($request->isMethod(Request::METHOD_GET)) {
+            $burialView = $this->causeOfDeathFetcher->getViewById($id);
+
+            return $this->json($burialView);
+        }
 
         return $response;
     }
