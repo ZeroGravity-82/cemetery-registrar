3e55b088a19d7fda5c8a6a58882b5bf78a9b7558
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-07-11 14:37:03 +0700

    add event about cause of death removing

---
+++ tests/Cemetery/Registrar/Domain/Model/CauseOfDeath/CauseOfDeathRemovedTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Model\CauseOfDeath;
+
+use Cemetery\Registrar\Domain\Model\CauseOfDeath\CauseOfDeathId;
+use Cemetery\Registrar\Domain\Model\CauseOfDeath\CauseOfDeathName;
+use Cemetery\Registrar\Domain\Model\CauseOfDeath\CauseOfDeathRemoved;
+use Cemetery\Tests\Registrar\Domain\Model\EventTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class CauseOfDeathRemovedTest extends EventTest
+{
+    private CauseOfDeathId   $causeOfDeathId;
+    private CauseOfDeathName $causeOfDeathName;
+
+    public function setUp(): void
+    {
+        $this->causeOfDeathId   = new CauseOfDeathId('CD001');
+        $this->causeOfDeathName = new CauseOfDeathName('Асфиксия');
+        $this->event            = new CauseOfDeathRemoved(
+            $this->causeOfDeathId,
+            $this->causeOfDeathName,
+        );
+    }
+
+    public function testItSuccessfullyCreated(): void
+    {
+        $this->assertSame($this->causeOfDeathId, $this->event->causeOfDeathId());
+        $this->assertSame($this->causeOfDeathName, $this->event->causeOfDeathName());
+    }
+}
