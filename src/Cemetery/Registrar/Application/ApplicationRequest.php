fbd23841cfdc1a1d42b8231c4e3d29e9758e9119
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-08-05 15:48:34 +0700

    fix docblock for base app request class

--- src/Cemetery/Registrar/Application/ApplicationRequest.php
+++ src/Cemetery/Registrar/Application/ApplicationRequest.php
@@ -6,7 +6,6 @@ namespace Cemetery\Registrar\Application;
 
 /**
  * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
- * @see    https://github.com/omniti-labs/jsend
  */
 abstract class ApplicationRequest
 {
