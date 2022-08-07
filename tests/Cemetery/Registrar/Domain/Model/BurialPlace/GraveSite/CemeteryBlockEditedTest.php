fb3dbf491eadaed90eaae289d3ec6b86f72f22e8
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-08-07 11:51:36 +0700

    add event for cemetery block editing

---
+++ tests/Cemetery/Registrar/Domain/Model/BurialPlace/GraveSite/CemeteryBlockEditedTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Model\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\CemeteryBlockEdited;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\CemeteryBlockId;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\CemeteryBlockName;
+use Cemetery\Tests\Registrar\Domain\Model\EventTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class CemeteryBlockEditedTest extends EventTest
+{
+    private CemeteryBlockId   $cemeteryBlockId;
+    private CemeteryBlockName $cemeteryBlockName;
+
+    public function setUp(): void
+    {
+        $this->cemeteryBlockId   = new CemeteryBlockId('CD001');
+        $this->cemeteryBlockName = new CemeteryBlockName('Асфиксия');
+        $this->event             = new CemeteryBlockEdited(
+            $this->cemeteryBlockId,
+            $this->cemeteryBlockName,
+        );
+    }
+
+    public function testItSuccessfullyCreated(): void
+    {
+        $this->assertSame($this->cemeteryBlockId, $this->event->cemeteryBlockId());
+        $this->assertSame($this->cemeteryBlockName, $this->event->cemeteryBlockName());
+    }
+}
