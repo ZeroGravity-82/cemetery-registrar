11a7f6d37795e4dda8b57e4a9a7034fee28c04e9
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-08-07 11:58:51 +0700

    add event for cemetery block removing

---
+++ src/Cemetery/Registrar/Domain/Model/BurialPlace/GraveSite/CemeteryBlockRemoved.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\Model\Event;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class CemeteryBlockRemoved extends Event
+{
+    public function __construct(
+        private CemeteryBlockId   $cemeteryBlockId,
+        private CemeteryBlockName $cemeteryBlockName,
+    ) {
+        parent::__construct();
+    }
+
+    public function cemeteryBlockId(): CemeteryBlockId
+    {
+        return $this->cemeteryBlockId;
+    }
+
+    public function cemeteryBlockName(): CemeteryBlockName
+    {
+        return $this->cemeteryBlockName;
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/Model/BurialPlace/GraveSite/CemeteryBlockRemovedTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Model\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\CemeteryBlockId;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\CemeteryBlockName;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\CemeteryBlockRemoved;
+use Cemetery\Tests\Registrar\Domain\Model\EventTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class CemeteryBlockRemovedTest extends EventTest
+{
+    private CemeteryBlockId   $cemeteryBlockId;
+    private CemeteryBlockName $cemeteryBlockName;
+
+    public function setUp(): void
+    {
+        $this->cemeteryBlockId   = new CemeteryBlockId('CB001');
+        $this->cemeteryBlockName = new CemeteryBlockName('южный');
+        $this->event             = new CemeteryBlockRemoved(
+            $this->cemeteryBlockId,
+            $this->cemeteryBlockName,
+        );
+    }
+
+    public function testItSuccessfullyCreated(): void
+    {
+        $this->assertSame($this->cemeteryBlockId, $this->event->cemeteryBlockId());
+        $this->assertSame($this->cemeteryBlockName, $this->event->cemeteryBlockName());
+    }
+}
