46ad9d2787de97b09e98755a4f5716cb808c877b
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-08-08 12:50:33 +0700

    add events for grave site

---
+++ src/Cemetery/Registrar/Domain/Model/BurialPlace/GraveSite/GraveSiteCreated.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\Model\Event;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class GraveSiteCreated extends Event
+{
+    public function __construct(
+        private GraveSiteId       $graveSiteId,
+        private CemeteryBlockName $cemeteryBlockName,
+        private RowInBlock        $rowInBlock,
+        private PositionInRow     $positionInRow,
+    ) {
+        parent::__construct();
+    }
+
+    public function graveSiteId(): GraveSiteId
+    {
+        return $this->graveSiteId;
+    }
+
+    public function cemeteryBlockName(): CemeteryBlockName
+    {
+        return $this->cemeteryBlockName;
+    }
+
+    public function rowInBlock(): RowInBlock
+    {
+        return $this->rowInBlock;
+    }
+
+    public function positionInRow(): PositionInRow
+    {
+        return $this->positionInRow;
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/Model/BurialPlace/GraveSite/GraveSiteEditedTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Model\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\CemeteryBlockName;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\GraveSiteEdited;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\GraveSiteId;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\PositionInRow;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\RowInBlock;
+use Cemetery\Tests\Registrar\Domain\Model\EventTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class GraveSiteEditedTest extends EventTest
+{
+    private GraveSiteId       $graveSiteId;
+    private CemeteryBlockName $cemeteryBlockName;
+    private RowInBlock        $rowInBlock;
+    private PositionInRow     $positionInRow;
+
+    public function setUp(): void
+    {
+        $this->graveSiteId       = new GraveSiteId('CB001');
+        $this->cemeteryBlockName = new CemeteryBlockName('южный');
+        $this->rowInBlock        = new RowInBlock(5);
+        $this->positionInRow     = new PositionInRow(10);
+        $this->event             = new GraveSiteEdited(
+            $this->graveSiteId,
+            $this->cemeteryBlockName,
+            $this->rowInBlock,
+            $this->positionInRow,
+        );
+    }
+
+    public function testItSuccessfullyCreated(): void
+    {
+        $this->assertSame($this->graveSiteId, $this->event->graveSiteId());
+        $this->assertSame($this->cemeteryBlockName, $this->event->cemeteryBlockName());
+        $this->assertSame($this->rowInBlock, $this->event->rowInBlock());
+        $this->assertSame($this->positionInRow, $this->event->positionInRow());
+    }
+}
