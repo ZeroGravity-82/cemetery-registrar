529dc93e2f1eb412bfb2c04fc325af632d071d95
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-08-24 01:32:43 +0700

    [wip]

--- src/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/Dbal/Fetcher/DoctrineDbalNaturalPersonFetcher.php
+++ src/Cemetery/Registrar/Infrastructure/Persistence/Doctrine/Dbal/Fetcher/DoctrineDbalNaturalPersonFetcher.php
@@ -24,17 +24,20 @@ class DoctrineDbalNaturalPersonFetcher extends DoctrineDbalFetcher implements Na
 
     public function findAll(int $page, ?string $term = null, int $pageSize = self::DEFAULT_PAGE_SIZE): NaturalPersonList
     {
+        $sql  = $this->buildFindAllSql($page, $term, $pageSize);
+
+
         $queryBuilder = $this->connection->createQueryBuilder()
             ->select(
                 'np.id                                                             AS id',
                 'np.full_name                                                      AS fullName',
                 'np.address                                                        AS address',
                 'np.phone                                                          AS phone',
-//                'IF(
-//                    np.phone IS NOT NULL OR np.phone_additional IS NOT NULL,
-//                    CONCAT_WS(', ', np.phone, np.phone_additional),
-//                    NULL
-//                )                                                                  AS phone',
+                'IF(
+                    np.phone IS NOT NULL OR np.phone_additional IS NOT NULL,
+                    CONCAT_WS(', ', np.phone, np.phone_additional),
+                    NULL
+                )                                                                  AS phone',
                 'np.email                                                          AS email',
                 'np.born_at                                                        AS bornAt',           // TODO fix format
                 'np.place_of_birth                                                 AS placeOfBirth',
@@ -53,8 +56,8 @@ class DoctrineDbalNaturalPersonFetcher extends DoctrineDbalFetcher implements Na
             ->addOrderBy('np.deceased_details->>"$.diedAt"')
             ->setFirstResult(($page - 1) * $pageSize)
             ->setMaxResults($pageSize);
-        $this->appendJoins($queryBuilder);
-        $this->appendAndWhereLikeTerm($queryBuilder, $term);
+        $this->appendJoinsSql($sql);
+        $this->appendAndWhereLikeTerm($sql, $term);
         $this->setTermParameter($queryBuilder, $term);
 
         $listData = $queryBuilder
@@ -86,11 +89,163 @@ class DoctrineDbalNaturalPersonFetcher extends DoctrineDbalFetcher implements Na
             ->fetchFirstColumn()[0];
     }
 
-    private function appendJoins(QueryBuilder $queryBuilder): void
+
+
+
+
+
+    private function buildFindAllSql(int $page, ?string $term, int $pageSize): string
     {
-        $queryBuilder
-            ->leftJoin('np', 'cause_of_death', 'cd', 'np.deceased_details->>"$.causeOfDeathId" = cd.id');
+        $sql = <<<FIND_ALL_SQL
+SELECT np.id                                                                                  AS id,
+       np.full_name                                                                           AS fullName,
+       np.address                                                                             AS address,
+       np.phone                                                                               AS phone,
+       np.phone_additional                                                                    AS phoneAdditional,
+       IF(
+          np.phone IS NOT NULL OR np.phone_additional IS NOT NULL,
+          CONCAT_WS(', ', np.phone, np.phone_additional),
+          NULL
+       )                                                                                      AS phoneComposed,
+       np.email                                                                               AS email,
+       IF(np.born_at IS NOT NULL, DATE_FORMAT(np.born_at, '%d.%m.%Y'), NULL)                  AS bornAtFormatted,
+       np.place_of_birth                                                                      AS placeOfBirth,
+       IF(np.passport IS NOT NULL, np.passport->>"$.series", NULL)                            AS passportSeries,
+       IF(np.passport IS NOT NULL, np.passport->>"$.number", NULL)                            AS passportNumber,
+       IF(
+          np.passport IS NOT NULL AND np.passport->>"$.issuedAt" IS NOT NULL,
+          DATE_FORMAT(np.passport->>"$.issuedAt", '%d.%m.%Y'),
+          NULL
+       )                                                                                      AS passportIssuedAtFormatted,
+       IF(np.passport IS NOT NULL, np.passport->>"$.issuedBy", NULL)                          AS passportIssuedBy,
+       IF(np.passport IS NOT NULL, np.passport->>"$.divisionCode", NULL)                      AS passportDivisionCode,
+       IF(
+          np.passport IS NOT NULL,
+          CONCAT(
+             np.passport->>"$.series",
+             ' № ',
+             np.passport->>"$.number",
+             ' выдан ',
+             np.passport->>"$.issuedBy",
+             ' ',
+             DATE_FORMAT(np.passport->>"$.issuedAt", '%d.%m.%Y'),
+             IF(
+                np.passport->>"$.divisionCode" IS NOT NULL,
+                CONCAT(' (', np.passport->>"$.divisionCode", ')'),
+                ''
+             )
+          ),
+          NULL
+       )                                                                                      AS passportComposed,
+       IF(
+          np.deceased_details IS NOT NULL AND np.deceased_details->>"$.diedAt" IS NOT NULL,
+          DATE_FORMAT(np.deceased_details->>"$.diedAt", '%d.%m.%Y'),
+          NULL
+       )                                                                                      AS diedAtFormatted,
+       IF(np.deceased_details IS NOT NULL, np.deceased_details->>"$.age", NULL)               AS age,
+       IF(
+          np.born_at IS NOT NULL AND np.deceased_details IS NOT NULL AND np.deceased_details->>"$.diedAt" IS NOT NULL,
+          TIMESTAMPDIFF(YEAR, np.born_at, np.deceased_details->>"$.diedAt"),
+          NULL
+       )                                                                                      AS ageCalculated,
+       cd.name                                                                                AS causeOfDeathName,
+       IF(
+          np.deceased_details IS NOT NULL AND np.deceased_details->>"$.deathCertificate" IS NOT NULL,
+          np.deceased_details->>"$.deathCertificate.series",
+          NULL
+       )                                                                                      AS deathCertificateSeries,
+       IF(
+          np.deceased_details IS NOT NULL AND np.deceased_details->>"$.deathCertificate" IS NOT NULL,
+          np.deceased_details->>"$.deathCertificate.number",
+          NULL
+       )                                                                                      AS deathCertificateNumber,
+       IF(
+          np.deceased_details IS NOT NULL AND np.deceased_details->>"$.deathCertificate" IS NOT NULL,
+          DATE_FORMAT(np.deceased_details->>"$.deathCertificate.issuedAt", '%d.%m.%Y'),
+          NULL
+       )                                                                                      AS deathCertificateIssuedAtFormatted,
+       IF(
+           np.deceased_details IS NOT NULL AND np.deceased_details->>"$.deathCertificate" IS NOT NULL,
+           CONCAT(
+               np.deceased_details->>"$.deathCertificate.series",
+               ' № ',
+               np.deceased_details->>"$.deathCertificate.number",
+               ' от ',
+               DATE_FORMAT(np.deceased_details->>"$.deathCertificate.issuedAt", '%d.%m.%Y')
+           ),
+           NULL
+       )                                                                                      AS deathCertificateComposed,
+       IF(
+          np.deceased_details IS NOT NULL AND np.deceased_details->>"$.cremationCertificate" IS NOT NULL,
+          np.deceased_details->>"$.cremationCertificate.number",
+          NULL
+       )                                                                                      AS cremationCertificateNumber,
+       IF(
+          np.deceased_details IS NOT NULL AND np.deceased_details->>"$.cremationCertificate" IS NOT NULL,
+          DATE_FORMAT(np.deceased_details->>"$.cremationCertificate.issuedAt", '%d.%m.%Y'),
+          NULL
+       )                                                                                      AS cremationCertificateIssuedAtFormatted,
+       IF(
+           np.deceased_details IS NOT NULL AND np.deceased_details->>"$.cremationCertificate" IS NOT NULL,
+           CONCAT(
+               np.deceased_details->>"$.cremationCertificate.number",
+               ' от ',
+               DATE_FORMAT(np.deceased_details->>"$.cremationCertificate.issuedAt", '%d.%m.%Y')
+           ),
+           NULL
+       )                                                                                      AS cremationCertificateComposed
+FROM natural_person AS np
+FIND_ALL_SQL;
+
+        $sql = $this->appendJoinsSql($sql);
+        $sql = $this->appendWhereRemovedAtIsNullSql($sql);
+        $sql = $this->appendAndWhereLikeTermSql($sql, $term);
+        $sql = $this->appendOrderByName($sql);
+
+        return $this->appendLimitOffset($sql, $page, $pageSize);
     }
+
+    private function appendJoinsSql(string $sql): string
+    {
+        return $sql . ' LEFT JOIN cause_of_death AS cd ON np.deceased_details->>"$.causeOfDeathId" = cd.id';
+    }
+
+    private function appendWhereRemovedAtIsNullSql(string $sql): string
+    {
+        return $sql . ' WHERE removedAt IS NULL';
+    }
+
+    private function appendAndWhereLikeTermSql(string $sql, ?string $term): string
+    {
+        if ($this->isTermNotEmpty($term)) {
+            $sql .= <<<LIKE_TERM_SQL
+  AND (fullName                              LIKE :term
+    OR address                               LIKE :term
+    OR phone                                 LIKE :term
+    OR phoneAdditional                       LIKE :term
+    OR email                                 LIKE :term
+    OR bornAtFormatted                       LIKE :term
+    OR placeOfBirth                          LIKE :term
+    OR passportSeries                        LIKE :term
+    OR passportNumber                        LIKE :term
+    OR passportIssuedAtFormatted             LIKE :term
+    OR passportIssuedBy                      LIKE :term
+    OR passportDivisionCode                  LIKE :term
+    OR diedAtFormatted                       LIKE :term
+    OR age                                   LIKE :term
+    OR ageCalculated                         LIKE :term
+    OR causeOfDeathName                      LIKE :term
+    OR deathCertificateSeries                LIKE :term
+    OR deathCertificateNumber                LIKE :term
+    OR deathCertificateIssuedAtFormatted     LIKE :term
+    OR cremationCertificateNumber            LIKE :term
+    OR cremationCertificateIssuedAtFormatted LIKE :term)
+LIKE_TERM_SQL;
+        }
+
+        return $sql;
+    }
+
 
     private function appendAndWhereLikeTerm(QueryBuilder $queryBuilder, ?string $term): void
     {
@@ -128,12 +283,12 @@ class DoctrineDbalNaturalPersonFetcher extends DoctrineDbalFetcher implements Na
                 $listItemData['id'],
                 $listItemData['fullName'],
                 $listItemData['address'],
-                $listItemData['phone'],
+                $listItemData['phoneComposed'],
                 $listItemData['email'],
-                $listItemData['bornAt'],
+                $listItemData['bornAtFormatted'],
                 $listItemData['placeOfBirth'],
-                $listItemData['passport'],
-                $listItemData['diedAt'],
+                $listItemData['passportComposed'],
+                $listItemData['diedAtFormatted'],
                 match ($listItemData['age']) {
                     'null'  => match ($listItemData['ageCalculated']) {
                             'null'  => null,
@@ -142,8 +297,8 @@ class DoctrineDbalNaturalPersonFetcher extends DoctrineDbalFetcher implements Na
                     default => (int) $listItemData['age'],
                 },
                 $listItemData['causeOfDeathName'],
-                $listItemData['deathCertificate'],
-                $listItemData['cremationCertificate'],
+                $listItemData['deathCertificateComposed'],
+                $listItemData['cremationCertificateComposed'],
             );
         }
 
