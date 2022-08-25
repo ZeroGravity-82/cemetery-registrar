005e6455873426a19844f64035a9c9d207c948c9
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-08-25 12:57:32 +0700

    add validation for cemetery block existence by name

--- src/Cemetery/Registrar/Application/BurialPlace/GraveSite/CemeteryBlockRequestValidator.php
+++ src/Cemetery/Registrar/Application/BurialPlace/GraveSite/CemeteryBlockRequestValidator.php
@@ -6,16 +6,28 @@ namespace Cemetery\Registrar\Application\BurialPlace\GraveSite;
 
 use Cemetery\Registrar\Application\ApplicationRequest;
 use Cemetery\Registrar\Application\ApplicationRequestValidator;
+use Cemetery\Registrar\Domain\View\BurialPlace\GraveSite\CemeteryBlockFetcher;
 
 /**
  * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
  */
 abstract class CemeteryBlockRequestValidator extends ApplicationRequestValidator
 {
+    public function __construct(
+        private readonly CemeteryBlockFetcher $cemeteryBlockFetcher,
+    ) {
+        parent::__construct();
+    }
     protected function validateName(ApplicationRequest $request): self
     {
         if ($request->name === null || empty(\trim($request->name))) {
             $this->note->addError('name', 'Квартал не может иметь пустое наименование.');
+        }
+        if (
+            $request->name !== null &&
+            $this->cemeteryBlockFetcher->doesExistByName($request->name)
+        ) {
+            $this->note->addError('name', 'Квартал с таким наименованием уже существует.');
         }
 
         return $this;
