91e32c578ff63f855d9d5146c02046f2036d4bde
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-08-25 12:49:16 +0700

    add validation for cause of death existence by name

--- src/Cemetery/Registrar/Application/CauseOfDeath/CauseOfDeathRequestValidator.php
+++ src/Cemetery/Registrar/Application/CauseOfDeath/CauseOfDeathRequestValidator.php
@@ -6,16 +6,29 @@ namespace Cemetery\Registrar\Application\CauseOfDeath;
 
 use Cemetery\Registrar\Application\ApplicationRequest;
 use Cemetery\Registrar\Application\ApplicationRequestValidator;
+use Cemetery\Registrar\Domain\View\CauseOfDeath\CauseOfDeathFetcher;
 
 /**
  * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
  */
 abstract class CauseOfDeathRequestValidator extends ApplicationRequestValidator
 {
+    public function __construct(
+        private readonly CauseOfDeathFetcher $causeOfDeathFetcher,
+    ) {
+        parent::__construct();
+    }
+
     protected function validateName(ApplicationRequest $request): self
     {
         if ($request->name === null || empty(\trim($request->name))) {
             $this->note->addError('name', 'Причина смерти не может иметь пустое наименование.');
+        }
+        if (
+            $request->name !== null &&
+            $this->causeOfDeathFetcher->doesExistByName($request->name)
+        ) {
+            $this->note->addError('name', 'Причина смерти с таким наименованием уже существует.');
         }
 
         return $this;
