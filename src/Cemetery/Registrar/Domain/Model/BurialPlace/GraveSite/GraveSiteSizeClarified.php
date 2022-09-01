bbe7bdd410b4ac84877bbeb54dfe6d413af6e089
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-09-01 16:31:36 +0700

    add event for grave site size clarification

---
+++ src/Cemetery/Registrar/Domain/Model/BurialPlace/GraveSite/GraveSiteSizeClarified.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\Model\Event;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class GraveSiteSizeClarified extends Event
+{
+    public function __construct(
+        private GraveSiteId    $graveSiteId,
+        private ?GraveSiteSize $size,
+    ) {
+        parent::__construct();
+    }
+
+    public function graveSiteId(): GraveSiteId
+    {
+        return $this->graveSiteId;
+    }
+
+    public function size(): ?GraveSiteSize
+    {
+        return $this->size;
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/Model/BurialPlace/GraveSite/GraveSiteSizeClarifiedTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Model\BurialPlace\GraveSite;
+
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\GraveSiteId;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\GraveSiteSize;
+use Cemetery\Registrar\Domain\Model\BurialPlace\GraveSite\GraveSiteSizeClarified;
+use Cemetery\Tests\Registrar\Domain\Model\EventTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class GraveSiteSizeClarifiedTest extends EventTest
+{
+    private GraveSiteId    $graveSiteId;
+    private ?GraveSiteSize $size;
+
+    public function setUp(): void
+    {
+        $this->graveSiteId = new GraveSiteId('GS001');
+        $this->size        = new GraveSiteSize('2.5');
+        $this->event       = new GraveSiteSizeClarified(
+            $this->graveSiteId,
+            $this->size,
+        );
+    }
+
+    public function testItSuccessfullyCreated(): void
+    {
+        $this->assertTrue($this->graveSiteId->isEqual($this->event->graveSiteId()));
+        $this->assertTrue($this->size->isEqual($this->event->size()));
+    }
+
+    public function testItSuccessfullyCreatedWithoutSize(): void
+    {
+        $this->event = new GraveSiteSizeClarified(
+            $this->graveSiteId,
+            null,
+        );
+        $this->assertTrue($this->graveSiteId->isEqual($this->event->graveSiteId()));
+        $this->assertNull($this->event->size());
+    }
+}
