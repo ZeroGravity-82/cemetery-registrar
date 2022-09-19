066e619162cede93b10468d25604ffc7692ca463
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-09-19 16:54:57 +0700

    add event for natural person contact clarification

---
+++ src/Cemetery/Registrar/Domain/Model/NaturalPerson/NaturalPersonContactClarified.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Registrar\Domain\Model\NaturalPerson;
+
+use Cemetery\Registrar\Domain\Model\Contact\Address;
+use Cemetery\Registrar\Domain\Model\Contact\Email;
+use Cemetery\Registrar\Domain\Model\Contact\PhoneNumber;
+use Cemetery\Registrar\Domain\Model\Event;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class NaturalPersonContactClarified extends Event
+{
+    public function __construct(
+        private NaturalPersonId $id,
+        private ?PhoneNumber    $phone,
+        private ?PhoneNumber    $phoneAdditional,
+        private ?Address        $address,
+        private ?Email          $email,
+    ) {
+        parent::__construct();
+    }
+
+    public function id(): NaturalPersonId
+    {
+        return $this->id;
+    }
+
+    public function phone(): ?PhoneNumber
+    {
+        return $this->phone;
+    }
+
+    public function phoneAdditional(): ?PhoneNumber
+    {
+        return $this->phoneAdditional;
+    }
+
+    public function address(): ?Address
+    {
+        return $this->address;
+    }
+
+    public function email(): ?Email
+    {
+        return $this->email;
+    }
+}
---
+++ tests/Cemetery/Registrar/Domain/Model/NaturalPerson/NaturalPersonContactClarifiedTest.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cemetery\Tests\Registrar\Domain\Model\NaturalPerson;
+
+use Cemetery\Registrar\Domain\Model\Contact\Address;
+use Cemetery\Registrar\Domain\Model\Contact\Email;
+use Cemetery\Registrar\Domain\Model\Contact\PhoneNumber;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\NaturalPersonContactClarified;
+use Cemetery\Registrar\Domain\Model\NaturalPerson\NaturalPersonId;
+use Cemetery\Tests\Registrar\Domain\Model\EventTest;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+class NaturalPersonContactClarifiedTest extends EventTest
+{
+    private NaturalPersonId $id;
+    private PhoneNumber     $phone;
+    private PhoneNumber     $phoneAdditional;
+    private Address         $address;
+    private Email           $email;
+
+    public function setUp(): void
+    {
+        $this->id              = new NaturalPersonId('NP001');
+        $this->phone           = new PhoneNumber('+7-111-111-11-11');
+        $this->phoneAdditional = new PhoneNumber('+7-222-222-22-22');
+        $this->address         = new Address('Новосибирск, Ленина 1');
+        $this->email           = new Email('some@email.com');
+        $this->event           = new NaturalPersonContactClarified(
+            $this->id,
+            $this->phone,
+            $this->phoneAdditional,
+            $this->address,
+            $this->email,
+        );
+    }
+
+    public function testItSuccessfullyCreated(): void
+    {
+        $this->assertTrue($this->id->isEqual($this->event->id()));
+        $this->assertTrue($this->phone->isEqual($this->event->phone()));
+        $this->assertTrue($this->phoneAdditional->isEqual($this->event->phoneAdditional()));
+        $this->assertTrue($this->address->isEqual($this->event->address()));
+        $this->assertTrue($this->email->isEqual($this->event->email()));
+    }
+
+    public function testItSuccessfullyCreatedWithoutPhone(): void
+    {
+        $this->event = new NaturalPersonContactClarified(
+            $this->id,
+            null,
+            $this->phoneAdditional,
+            $this->address,
+            $this->email,
+        );
+
+        $this->assertTrue($this->id->isEqual($this->event->id()));
+        $this->assertNull($this->event->phone());
+        $this->assertTrue($this->phoneAdditional->isEqual($this->event->phoneAdditional()));
+        $this->assertTrue($this->address->isEqual($this->event->address()));
+        $this->assertTrue($this->email->isEqual($this->event->email()));
+    }
+
+    public function testItSuccessfullyCreatedWithoutPhoneAdditional(): void
+    {
+        $this->event = new NaturalPersonContactClarified(
+            $this->id,
+            $this->phone,
+            null,
+            $this->address,
+            $this->email,
+        );
+
+        $this->assertTrue($this->id->isEqual($this->event->id()));
+        $this->assertTrue($this->phone->isEqual($this->event->phone()));
+        $this->assertNull($this->event->phoneAdditional());
+        $this->assertTrue($this->address->isEqual($this->event->address()));
+        $this->assertTrue($this->email->isEqual($this->event->email()));
+    }
+
+    public function testItSuccessfullyCreatedWithoutAddress(): void
+    {
+        $this->event = new NaturalPersonContactClarified(
+            $this->id,
+            $this->phone,
+            $this->phoneAdditional,
+            null,
+            $this->email,
+        );
+
+        $this->assertTrue($this->id->isEqual($this->event->id()));
+        $this->assertTrue($this->phone->isEqual($this->event->phone()));
+        $this->assertTrue($this->phoneAdditional->isEqual($this->event->phoneAdditional()));
+        $this->assertNull($this->event->address());
+        $this->assertTrue($this->email->isEqual($this->event->email()));
+    }
+
+    public function testItSuccessfullyCreatedWithoutEmail(): void
+    {
+        $this->event = new NaturalPersonContactClarified(
+            $this->id,
+            $this->phone,
+            $this->phoneAdditional,
+            $this->address,
+            null,
+        );
+
+        $this->assertTrue($this->id->isEqual($this->event->id()));
+        $this->assertTrue($this->phone->isEqual($this->event->phone()));
+        $this->assertTrue($this->phoneAdditional->isEqual($this->event->phoneAdditional()));
+        $this->assertTrue($this->address->isEqual($this->event->address()));
+        $this->assertNull($this->event->email());
+    }
+}
