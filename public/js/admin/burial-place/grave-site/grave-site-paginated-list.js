6034632dc98648480caf129d052e2c0b89b68a35
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-10-15 13:01:07 +0700

    fix JS for grave site list

--- public/js/admin/burial-place/grave-site/grave-site-paginated-list.js
+++ public/js/admin/burial-place/grave-site/grave-site-paginated-list.js
@@ -1,28 +1,12 @@
 `use strict`;
 
-const $modalContainer = $(`.grave-site-modal-container`);
-const graveSiteCard   = new GraveSiteCard(
-  $modalContainer,
-  spinner,
-  window.GRAVE_SITE_PROPS,
-);
-// graveSiteCard.show(`GS001`);
-const graveSiteForm = new GraveSiteForm(
-  $modalContainer,
-  spinner,
-  window.GRAVE_SITE_PROPS,
-);
-graveSiteForm.show(`NEW`);
-
 const $tableGraveSite     = $(`#graveSiteList`);
-// const $createGraveSiteBtn = $(`.js-create-grave-site-btn`);
+const $createGraveSiteBtn = $(`.js-create-grave-site-btn`);
+const $modalContainer     = $(`.grave-site-modal-container`);
+const graveSiteForm       = new GraveSiteForm($modalContainer, spinner, window.GRAVE_SITE_PROPS);
+const graveSiteCard       = new GraveSiteCard($modalContainer, spinner, window.GRAVE_SITE_PROPS);
 
-// // Create
-// $createGraveSiteBtn.on(`click`, () => {
-//   graveSiteFormNew_show();
-// });
-
-// Show
+$createGraveSiteBtn.on(`click`, () => graveSiteForm.show(`NEW`));
 $tableGraveSite.on(`click`, `td`, (e) => {
   const graveSiteId = $(e.target).closest(`tr`).attr(`data-id`);
   graveSiteCard.show(graveSiteId);
