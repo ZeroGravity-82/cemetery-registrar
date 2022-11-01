1531a45703df80b34404a5a2593409f8de2522b0
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-11-01 12:39:48 +0700

    fix class hierarchy for Fetcher

--- src/Cemetery/Registrar/Infrastructure/Persistence/Fetcher.php
+++ src/Cemetery/Registrar/Infrastructure/Persistence/Fetcher.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Cemetery\Registrar\Infrastructure\Persistence;
 
+use Cemetery\Registrar\Domain\View\Fetcher as FetcherInterface;
+
 /**
  * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
  */
-abstract class Fetcher
+abstract class Fetcher implements FetcherInterface
 {
     protected function isTermNotEmpty(?string $term): bool
     {
