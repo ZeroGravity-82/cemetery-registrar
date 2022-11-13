b52b7875f42ba6aae6b879c41fb5b7817c916617
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2022-11-13 12:10:40 +0700

    [wip]

--- public/js/NaturalPersonForm.js
+++ public/js/NaturalPersonForm.js
@@ -88,14 +88,29 @@ class NaturalPersonForm extends Form {
 <form></form>`).append($(`
   <div class="container"></div>`).append(
     this._renderFormRowForFullName()).append(
+
     this._renderFormRowForPhone()).append(
     this._renderFormRowForPhoneAdditional()).append(
     this._renderFormRowForAddress()).append(
     this._renderFormRowForEmail()).append(
+
     this._renderFormRowForBornAt()).append(
     this._renderFormRowForPlaceOfBirth()).append(
-    this._renderFormRowForPassport()).append(
-    this._renderFormRowForDeceasedDetails())).append(
+
+    this._renderFormRowForPassportSeries()).append(
+    this._renderFormRowForPassportNumber()).append(
+    this._renderFormRowForPassportIssuedAt()).append(
+    this._renderFormRowForPassportIssuedBy()).append(
+    this._renderFormRowForPassportDivisionCode()).append(
+
+    this._renderFormRowForDeceasedDetailsDiedAt()).append(
+    this._renderFormRowForDeceasedDetailsAge()).append(
+    this._renderFormRowForDeceasedDetailsCauseOfDeath()).append(
+    this._renderFormRowForDeceasedDetailsDeathCertificateSeries()).append(
+    this._renderFormRowForDeceasedDetailsDeathCertificateNumber()).append(
+    this._renderFormRowForDeceasedDetailsDeathCertificateIssuedAt()).append(
+    this._renderFormRowForDeceasedDetailsCremationCertificateNumber()).append(
+    this._renderFormRowForDeceasedDetailsCremationCertificateIssuedAt())).append(
   this.dom.$formButtons);
   }
   _renderFormRowForFullName(fullName = null) {
@@ -103,7 +118,7 @@ class NaturalPersonForm extends Form {
 <input type="text" class="form-control form-control-sm"
        id="fullName" name="fullName"
        aria-describedby="fullNameFeedback"
-       aria-label="ФИО"
+       aria-label="Фамилия, имя, отчество"
        value="${fullName ?? ``}">
     `);
 
@@ -137,7 +152,7 @@ class NaturalPersonForm extends Form {
 <input type="text" class="form-control form-control-sm"
        id="phoneAdditional" name="phoneAdditional"
        aria-describedby="phoneAdditionalFeedback"
-       aria-label="Доп. телефон"
+       aria-label="Дополнительный телефон"
        value="${phoneAdditional ?? ``}">
     `);
 
@@ -173,7 +188,7 @@ class NaturalPersonForm extends Form {
 <input type="text" class="form-control form-control-sm"
        id="email" name="email"
        aria-describedby="emailFeedback"
-       aria-label="Эл. почта"
+       aria-label="Электронная почта"
        value="${email ?? ``}">
     `);
 
@@ -191,7 +206,7 @@ class NaturalPersonForm extends Form {
        id="bornAt" name="bornAt"
        aria-describedby="bornAtFeedback"
        aria-label="Дата рождения"
-       value="${bornAt.split(`.`).reverse().join(`-`) ?? ``}">
+       value="${bornAt ? bornAt.split(`.`).reverse().join(`-`) : ``}">
     `);
 
     return $(`
@@ -219,17 +234,227 @@ class NaturalPersonForm extends Form {
     <div id="placeOfBirthFeedback" class="invalid-feedback"></div>
     `)));
   }
-  _renderFormRowForPassport() {
-    // TODO
+  _renderFormRowForPassportSeries(passportSeries = null) {
+    this.dom.$passportSeriesInput = $(`
+<input type="text" class="form-control form-control-sm"
+       id="passportSeries" name="passportSeries"
+       aria-describedby="passportSeriesFeedback"
+       aria-label="Серия паспорта"
+       value="${passportSeries ?? ``}">
+    `);
+
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="passportSeries" class="form-label">Серия</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$passportSeriesInput).append($(`
+    <div id="passportSeriesFeedback" class="invalid-feedback"></div>
+    `)));
   }
-  _renderFormRowForDeceasedDetails() {
-    // TODO
+  _renderFormRowForPassportNumber(passportNumber = null) {
+    this.dom.$passportNumberInput = $(`
+<input type="text" class="form-control form-control-sm"
+       id="passportNumber" name="passportNumber"
+       aria-describedby="passportNumberFeedback"
+       aria-label="Номер паспорта"
+       value="${passportNumber ?? ``}">
+    `);
+
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="passportNumber" class="form-label">Номер</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$passportNumberInput).append($(`
+    <div id="passportNumberFeedback" class="invalid-feedback"></div>
+    `)));
   }
+  _renderFormRowForPassportIssuedAt(passportIssuedAt = null) {
+    this.dom.$passportIssuedAtInput = $(`
+<input type="date" class="form-control form-control-sm"
+       id="passportIssuedAt" name="passportIssuedAt"
+       aria-describedby="passportIssuedAtFeedback"
+       aria-label="Дата выдачи паспорта"
+       value="${passportIssuedAt ? passportIssuedAt.split(`-`).reverse().join(`.`) : ``}">
+    `);
 
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="passportIssuedAt" class="form-label">Дата выдачи</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$passportIssuedAtInput).append($(`
+    <div id="passportIssuedAtFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForPassportIssuedBy(passportIssuedBy = null) {
+    this.dom.$passportIssuedByInput = $(`
+<input type="text" class="form-control form-control-sm"
+       id="passportIssuedBy" name="passportIssuedBy"
+       aria-describedby="passportIssuedByFeedback"
+       aria-label="Кем выдан паспорт"
+       value="${passportIssuedBy ?? ``}">
+    `);
 
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="passportIssuedBy" class="form-label">Кем выдан</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$passportIssuedByInput).append($(`
+    <div id="passportIssuedByFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForPassportDivisionCode(passportDivisionCode = null) {
+    this.dom.$passportDivisionCodeInput = $(`
+<input type="text" class="form-control form-control-sm"
+       id="passportDivisionCode" name="passportDivisionCode"
+       aria-describedby="passportDivisionCodeFeedback"
+       aria-label="Код подразделения паспорта"
+       value="${passportDivisionCode ?? ``}">
+    `);
 
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="passportDivisionCode" class="form-label">Код подразделения</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$passportDivisionCodeInput).append($(`
+    <div id="passportDivisionCodeFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForDeceasedDetailsDiedAt(deceasedDetailsDiedAt = null) {
+    this.dom.$deceasedDetailsDiedAtInput = $(`
+<input type="date" class="form-control form-control-sm"
+       id="deceasedDetailsDiedAt" name="deceasedDetailsDiedAt"
+       aria-describedby="deceasedDetailsDiedAtFeedback"
+       aria-label="Дата смерти"
+       value="${deceasedDetailsDiedAt ? deceasedDetailsDiedAt.split(`-`).reverse().join(`.`) : ``}">
+    `);
 
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="deceasedDetailsDiedAt" class="form-label">Дата смерти</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$deceasedDetailsDiedAtInput).append($(`
+    <div id="deceasedDetailsDiedAtFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForDeceasedDetailsAge(deceasedDetailsAge = null) {
+    this.dom.$deceasedDetailsAgeInput = $(`
+<input type="number" min="0" class="form-control form-control-sm"
+       id="deceasedDetailsAge" name="deceasedDetailsAge"
+       aria-describedby="deceasedDetailsAgeFeedback"
+       aria-label="Возраст"
+       value="${deceasedDetailsAge ?? ``}">
+    `);
 
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="deceasedDetailsAge" class="form-label">Возраст</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$deceasedDetailsAgeInput).append($(`
+    <div id="deceasedDetailsAgeFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForDeceasedDetailsCauseOfDeath(deceasedDetailsCauseOfDeath = null) {
+    this.dom.$deceasedDetailsCauseOfDeathInput = $(`
+<input type="text" class="form-control form-control-sm"
+       id="deceasedDetailsCauseOfDeath" name="deceasedDetailsCauseOfDeath"
+       aria-describedby="deceasedDetailsCauseOfDeathFeedback"
+       aria-label="Причина смерти"
+       value="${deceasedDetailsCauseOfDeath ?? ``}">
+    `);
+
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="deceasedDetailsCauseOfDeath" class="form-label">Причина смерти</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$deceasedDetailsCauseOfDeathInput).append($(`
+    <div id="deceasedDetailsCauseOfDeathFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForDeceasedDetailsDeathCertificateSeries(deceasedDetailsDeathCertificateSeries = null) {
+    this.dom.$deceasedDetailsDeathCertificateSeriesInput = $(`
+<input type="text" class="form-control form-control-sm"
+       id="deceasedDetailsDeathCertificateSeries" name="deceasedDetailsDeathCertificateSeries"
+       aria-describedby="deceasedDetailsDeathCertificateSeriesFeedback"
+       aria-label="Серия свидетельства о смерти"
+       value="${deceasedDetailsDeathCertificateSeries ?? ``}">
+    `);
+
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="deceasedDetailsDeathCertificateSeries" class="form-label">Серия</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$deceasedDetailsDeathCertificateSeriesInput).append($(`
+    <div id="deceasedDetailsDeathCertificateSeriesFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForDeceasedDetailsDeathCertificateNumber(deceasedDetailsDeathCertificateNumber = null) {
+    this.dom.$deceasedDetailsDeathCertificateNumberInput = $(`
+<input type="text" class="form-control form-control-sm"
+       id="deceasedDetailsDeathCertificateNumber" name="deceasedDetailsDeathCertificateNumber"
+       aria-describedby="deceasedDetailsDeathCertificateNumberFeedback"
+       aria-label="Номер свидетельства о смерти"
+       value="${deceasedDetailsDeathCertificateNumber ?? ``}">
+    `);
+
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="deceasedDetailsDeathCertificateNumber" class="form-label">Номер</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$deceasedDetailsDeathCertificateNumberInput).append($(`
+    <div id="deceasedDetailsDeathCertificateNumberFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForDeceasedDetailsDeathCertificateIssuedAt(deceasedDetailsDeathCertificateIssuedAt = null) {
+    this.dom.$deceasedDetailsDeathCertificateIssuedAtInput = $(`
+<input type="date" class="form-control form-control-sm"
+       id="deceasedDetailsDeathCertificateIssuedAt" name="deceasedDetailsDeathCertificateIssuedAt"
+       aria-describedby="deceasedDetailsDeathCertificateIssuedAtFeedback"
+       aria-label="Дата выдачи свидетельства о смерти"
+       value="${deceasedDetailsDeathCertificateIssuedAt ? deceasedDetailsDeathCertificateIssuedAt.split(`-`).reverse().join(`.`) : ``}">
+    `);
+
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="deceasedDetailsDeathCertificateIssuedAt" class="form-label">Дата выдачи</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$deceasedDetailsDeathCertificateIssuedAtInput).append($(`
+    <div id="deceasedDetailsDeathCertificateIssuedAtFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForDeceasedDetailsCremationCertificateNumber(deceasedDetailsCremationCertificateNumber = null) {
+    this.dom.$deceasedDetailsCremationCertificateNumberInput = $(`
+<input type="text" class="form-control form-control-sm"
+       id="deceasedDetailsCremationCertificateNumber" name="deceasedDetailsCremationCertificateNumber"
+       aria-describedby="deceasedDetailsCremationCertificateNumberFeedback"
+       aria-label="Номер справки о кремации"
+       value="${deceasedDetailsCremationCertificateNumber ?? ``}">
+    `);
+
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="deceasedDetailsCremationCertificateNumber" class="form-label">Номер</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$deceasedDetailsCremationCertificateNumberInput).append($(`
+    <div id="deceasedDetailsCremationCertificateNumberFeedback" class="invalid-feedback"></div>
+    `)));
+  }
+  _renderFormRowForDeceasedDetailsCremationCertificateIssuedAt(deceasedDetailsCremationCertificateIssuedAt = null) {
+    this.dom.$deceasedDetailsCremationCertificateIssuedAtInput = $(`
+<input type="date" class="form-control form-control-sm"
+       id="deceasedDetailsCremationCertificateIssuedAt" name="deceasedDetailsCremationCertificateIssuedAt"
+       aria-describedby="deceasedDetailsCremationCertificateIssuedAtFeedback"
+       aria-label="Дата выдачи справки о кремации"
+       value="${deceasedDetailsCremationCertificateIssuedAt ? deceasedDetailsCremationCertificateIssuedAt.split(`-`).reverse().join(`.`) : ``}">
+    `);
+
+    return $(`
+<div class="row pb-2"></div>`).append($(`
+  <div class="col-md-3 px-0"><label for="deceasedDetailsCremationCertificateIssuedAt" class="form-label">Дата выдачи</label></div>`)).append($(`
+  <div class="col-md-9 px-0">`).append(
+        this.dom.$deceasedDetailsCremationCertificateIssuedAtInput).append($(`
+    <div id="deceasedDetailsCremationCertificateIssuedAtFeedback" class="invalid-feedback"></div>
+    `)));
+  }
   _renderClarifyFullName() {
     this.dom.$form = $(`
 <form></form>`).append($(`
